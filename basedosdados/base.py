6181a9f88c228ae871a6c23cdf3114a1f56b1b27
d116626 <d116626@gmail.com>
2020-10-16 12:37:38 -0300

    client start with credentials

--- basedosdados/base.py
+++ basedosdados/base.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery, storage
+from google.oauth2 import service_account
 import yaml
 from jinja2 import Template
 from pathlib import Path
@@ -30,22 +31,29 @@ class Base:
         self.bucket_name = bucket_name or self.config["bucket_name"]
         self.uri = f"gs://{self.bucket_name}" + "/staging/{dataset}/{table}/*"
 
+    def _load_credentials(self, mode):
+
+        return service_account.Credentials.from_service_account_file(
+            self.config["gcloud-projects"][mode]['credentials_path']+ f"/{mode}.json",
+            scopes=["https://www.googleapis.com/auth/cloud-platform"])
+
+        
     @property
     @lru_cache(256)
     def client(self):
 
         return dict(
             bigquery_prod=bigquery.Client(
-                # credentials=self._load_credentials("prod"),
-                project=self.config["gcloud-projects"]["prod"],
+                credentials=self._load_credentials('prod'),
+                project=self.config["gcloud-projects"]["prod"]['name'],
             ),
             bigquery_staging=bigquery.Client(
-                # credentials=self._load_credentials("staging"),
-                project=self.config["gcloud-projects"]["staging"],
+                credentials=self._load_credentials('staging'),
+                project=self.config["gcloud-projects"]["staging"]['name'],
             ),
             storage_staging=storage.Client(
-                # credentials=self._load_credentials("staging"),
-                project=self.config["gcloud-projects"]["staging"],
+                credentials=self._load_credentials('staging'),
+                project=self.config["gcloud-projects"]["staging"]['name'],
             ),
         )
 
@@ -87,7 +95,6 @@ class Base:
             c_file = tomlkit.parse(
                 (Path(__file__).parent / "configs/config.toml").open("r").read()
             )
-
             while True:
                 res = (
                     input(
@@ -99,14 +106,14 @@ class Base:
                     .lower()
                     .strip()
                 )
-
+                if res=='':
+                    res='y'
+                    
                 if res == "y":
-
                     metadata_path = Path.cwd()
                     break
                 elif res == "n":
                     metadata_path = input("\nWhere would you like to save it:\n")
-
                     break
                 else:
                     print(f"{res} is not accepted as an awnser. Try y or n.\n")
@@ -137,15 +144,55 @@ class Base:
                 "basedosdados-staging",
             )
 
+            while True:
+                credential_staging_path = f"{Path.home()}/.basedosdados/credentials"
+                credentials_url_staging = f"https://console.cloud.google.com/apis/credentials/serviceaccountkey?project={project_staging}"
+                res = (
+                    input(
+                        "\n********* STEP 5 **********\n"
+                        f"Save the JSON credential with the name staging.json from the link [{credentials_url_staging}]"
+                        "in the path of your choice. "
+                        f"Is it at the current path ({credential_staging_path})? [Y/n]\n"
+                    )
+                    .lower()
+                    .strip()
+                )
+                
+                if res=='':
+                    res='y'
+                    
+                if res == "y":
+                    credential_staging_path = credential_staging_path
+                    break
+                elif res == "n":
+                    credential_staging_path = input("\nWhere would you like to save it:\n")
+
+                    break
+                else:
+                    print(f"{res} is not accepted as an awnser. Try y or n.\n")
+
+            c_file["gcloud-projects"]['staging']['credentials_path'] = str(credential_staging_path)
+            
+            
             res = (
                 input(
-                    "\n********* STEP 5 **********\n"
-                    "Is your production project the same as the staging? [y/n]\n"
+                    "\n********* STEP 6 **********\n"
+                    "Is your production project the same as the staging? [y/N]\n"
                 )
                 .strip()
                 .lower()
             )
+            
+            if res=='':
+                res='n'
 
+            bucket_name = self._input_validator(
+                "\n********* STEP 7 **********\n"
+                "What is the Storage Bucket that you are going to be using to save the data?\n"
+                "Bucket name [basedosdados]: ",
+                "basedosdados",
+            )
+            
             while True:
                 if res == "y":
                     print("Great!")
@@ -160,18 +207,42 @@ class Base:
                     break
                 else:
                     print(f"{res} is not accepted as an awnser. Try y or n.\n")
+            
+            while True:
+                credential_prod_path = f"{Path.home()}/.basedosdados/credentials"
+                credentials_url_prod = f"https://console.cloud.google.com/apis/credentials/serviceaccountkey?project={project_prod}"
+                res = (
+                    input(
+                        "\n********* STEP 8 **********\n"
+                        f"Save the JSON credential with the name prod.json from the link {credentials_url_prod}"
+                        "in the path of your choice. "
+                        f"Is it at the current path ({credential_prod_path})? [Y/n]\n"
+                    )
+                    .lower()
+                    .strip()
+                )
+                
+                if res=='':
+                    res='y'
+                    
+                if res == "y":
+                    credential_prod_path = credential_prod_path
+                    break
+                elif res == "n":
+                    credential_prod_path = input("\nWhere would you like to save it:\n")
 
-            bucket_name = self._input_validator(
-                "\n********* STEP 6 **********\n"
-                "What is the Storage Bucket that you are going to be using to save the data?\n"
-                "Bucket name [basedosdados]: ",
-                "basedosdados",
-            )
+                    break
+                else:
+                    print(f"{res} is not accepted as an awnser. Try y or n.\n")
 
-            c_file["gcloud-projects"]["staging"] = project_staging
-            c_file["gcloud-projects"]["prod"] = project_prod
+            
+            c_file["gcloud-projects"]['prod']['credentials_path'] = str(credential_prod_path)
+            
+            
+            c_file["gcloud-projects"]["staging"]['name'] = project_staging
+            c_file["gcloud-projects"]["prod"]['name'] = project_prod
             c_file["bucket_name"] = project_prod
-            c_file["templates_path"] = str(self.config_path / "templates")
+            c_file["templates_path"] = f"{Path.home()}/.basedosdados/templates"
 
             config_file.open("w").write(tomlkit.dumps(c_file))
             self._refresh_templates()
