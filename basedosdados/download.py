0be54da6729b57c4da6b0e8c42d852b5224c0463
Andre Aguiar <ufabc.andre@gmail.com>
2020-10-17 01:36:06 -0300

    add direct download feature

--- basedosdados/download.py
+++ basedosdados/download.py
@@ -1,5 +1,6 @@
-from google.cloud import bigquery
+from google.cloud import bigquery, storage
 from pathlib import Path
+import time
 
 
 def download(
@@ -9,6 +10,7 @@ def download(
     table_id=None,
     project_id="basedosdados",
     limit=None,
+    bypass_memory=False,
     **pandas_kwargs,
 ):
     """Download table or query result from basedosdados BigQuery.
@@ -56,20 +58,24 @@ def download(
 
     savepath = Path(savepath)
 
+    if savepath.is_dir():
+        if table_id is not None:
+            savepath = savepath / (table_id + ".csv")
+        else:
+            savepath = savepath / ("query_result.csv")
+
     if (dataset_id is not None) and (table_id is not None):
-        table = read_table(dataset_id, table_id, project_id, limit=limit)
+        if bypass_memory:
+            _direct_download(dataset_id, table_id, savepath, project_id)
+            return
+        else:
+            table = read_table(dataset_id, table_id, project_id, limit=limit)
     elif query is not None:
         if limit is not None:
             query += f" limit {limit}"
         table = read_sql(query)
     elif query is None:
         raise Exception("Either table_id, dataset_id or query should be filled.")
-
-    if savepath.is_dir():
-        if table_id is not None:
-            savepath = savepath / (table_id + ".csv")
-        else:
-            savepath = savepath / ("query_result.csv")
 
     table.to_csv(savepath, **pandas_kwargs)
 
@@ -132,3 +138,160 @@ def read_table(dataset_id, table_id, project_id="basedosdados", limit=None):
         raise Exception("Both table_id and dataset_id should be filled.")
 
     return read_sql(query)
+
+def _direct_download(
+    dataset_id, 
+    table_id, 
+    savepath, 
+    project_id='basedosdados'):
+    """
+    Download file to disk without the requirement of loading it in memory.
+
+    Creates a temporary file based on the `table_id` and the time of 
+    execution. Also create a temporary bucket using the `dataset_id` 
+    and the time of execution. Move the table to the temporary file and 
+    download it to disk. In the end, remove the temporary bucket.
+    
+    Parameters
+    ----------
+    dataset_id : str
+        Dataset id available in basedosdados. 
+        It should always come with table_id.
+    table_id : str
+        Table id available in project_id.dataset_id.
+    savepath : str
+        Local path in which file should be stored in local disk.
+    project_id: str, optional
+        In case you want to use to query another project, by default 'basedosdados'
+        
+    """
+    
+    time_hash = str(hash(time.time()))
+    tmp_file_name = table_id + '_' + time_hash
+    tmp_bucket_name = dataset_id + '_' + time_hash
+
+    blob_path = f'gs://{tmp_bucket_name}/{tmp_file_name}'
+    
+    try: 
+        # create temporary bucket
+        _create_bucket(tmp_bucket_name)
+        
+        # move table to temporary file inside temporary bucket
+        _move_table_to_bucket(dataset_id, table_id, blob_path, project_id)
+
+        # download file from bucket directly to disk
+        _download_blob_from_bucket(tmp_bucket_name, tmp_file_name, savepath)
+    except Exception as err:
+        # TODO handle exceptions for 404 (not found), 403 (forbidden)
+        raise Exception(err)
+    finally: 
+        # delete temporary bucket (even in the case of crashing)
+        _delete_bucket(tmp_bucket_name)
+
+def _download_blob_from_bucket(
+    bucket_name : str, 
+    blob_name : str, 
+    savepath : str):
+    """
+    Download a blob from a bucket to the path specified.
+
+    Parameters
+    ----------
+    bucket_name : str
+        Name of the bucket for the file to be stored.
+    blob_name : str
+        Name of the file to be downloaded. 
+        A file stored in a bucket is called a blob. It can have several
+        formats such as `.csv`, `.avro`
+    savepath : str
+        Local path in which file should be stored in local disk.
+    """
+    savepath = Path(savepath)
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    
+    blob.download_to_filename(savepath)
+    
+ 
+def _create_bucket(bucket_name):
+    """
+    Create a new buket in a specific location with standard storage class
+
+    Parameters
+    ----------
+    bucket_name : str
+        Name of the bucket to be created.
+    
+    """
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    
+    # standard storage class are adequate for data
+    # stored for only brief periods of time
+    bucket.storage_class = 'STANDARD'
+    
+    new_bucket = storage_client.create_bucket(bucket, location='US')
+
+def _delete_bucket(bucket_name):
+    """
+    Forceably deletes a bucket. 
+
+    This method deletes all blobs from a bucket.
+
+    Parameters
+    ----------
+    bucket_name : str
+        Name of the bucket to be deleted.
+    
+    """
+    MAX_BLOBS = 256
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    # NOTE: force=True implementation will not delete 
+    # the temporary bucket if n_blobs >= 256
+    n_blobs = len(list(storage_client.list_blobs(bucket_name)))
+    if n_blobs >= MAX_BLOBS:
+        raise Exception(f"""Your temporary bucket contains more than {MAX_BLOBS}. 
+    You should manually delete it (force=True will not be able to delete it.).""")
+
+    bucket.delete(force=True)
+
+def _move_table_to_bucket(
+    dataset_id, 
+    table_id, 
+    blob_path, 
+    project_id='basedosdados'):
+    """
+    Move table from BigQuery to bucket
+
+    Parameters
+    ----------
+    dataset_id : str
+        Dataset id available in basedosdados. It should always come with table_id.
+    table_id : str
+        Table id available in project_id.dataset_id.
+    blob_path : str
+        Path in bucket where the table will be stored. 
+        Called `destination_uri` in the documentation in the form of: 
+            `gs://<bucket_name>/<file_name.csv>
+    project_id: str, optional
+        In case you want to use to query another project, by default 'basedosdados'
+        
+    """
+    client = bigquery.Client()
+    dataset_ref = bigquery.DatasetReference(project_id, dataset_id)
+    table_ref = dataset_ref.table(table_id)
+    
+    # perform transfer from bq to bucket
+    extract_job = client.extract_table(
+        table_ref,
+        blob_path,
+        location='US'
+    )
+    # wait for API results
+    extract_job.result()
+
