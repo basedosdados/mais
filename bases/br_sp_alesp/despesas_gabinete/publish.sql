6760232cc5acc296da6c1d591c308e0e5cca9024
d116626 <d116626@gmail.com>
2020-10-21 19:11:49 -0300

    despesas de gabinete dos deputados estaduais de sp, 2002 a 2020

---
+++ bases/br_sp_alesp/despesas_gabinete/publish.sql
@@ -0,0 +1,30 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(matricula AS STRING) matricula,
+SAFE_CAST(deputado AS STRING) deputado,
+SAFE_CAST(cnpj AS STRING) cnpj,
+SAFE_CAST(fornecedor AS STRING) fornecedor,
+SAFE_CAST(tipo AS STRING) tipo,
+SAFE_CAST(valor AS INT64) valor
+from basedosdados-staging.br_sp_alesp_staging.despesas_gabinete as t
