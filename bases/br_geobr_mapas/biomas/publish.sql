12adb416d03f28841d666dda4f22f656e84e8b79
João Carabetta <joao.carabetta@gmail.com>
2020-12-01 17:28:24 -0300

    Update publish.sql

--- bases/br_geobr_mapas/biomas/publish.sql
+++ bases/br_geobr_mapas/biomas/publish.sql
@@ -23,5 +23,5 @@ SELECT
 SAFE_CAST(id_bioma AS STRING) id_bioma,
 SAFE_CAST(nome_bioma AS STRING) nome_bioma,
 SAFE_CAST(year AS INT64) ano,
-SAFE.ST_GEOGFROMTEXT(geometria) geometria
+SAFE.ST_GEOGFROMTEXT(geometria) geometria 
 from basedosdados-staging.br_geobr_mapas_staging.biomas as t
