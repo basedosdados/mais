7350ae4be06446ee2fa45c2635037eb9af49d71f
Pedro Cavalcante <pedrocolrj@gmail.com>
2021-01-24 21:24:38 -0300

    primeiras duas abordagens para baixar localmente tabelas bem como carregar na memória como tibbles

--- rbdd/R/python_port.R
+++ rbdd/R/python_port.R
@@ -35,26 +35,54 @@ set_billing_id <- function(billing_project_id) {
 
   }
 
-
   Sys.setenv(billing_project_id = billing_project_id,
              billing_project_set = TRUE)
 
 }
 
 
+#' Download a table locally
+#'
+#' @param query a string contaning valid a SQL query.
+#' @param savepath a path describing where the table should be saved. Defaults to the current working directory.
+#' @param billin_project_id a string containing your billing project id. If you've run `set_billing_id` then feel free to leave this empty.
 
 
 download <- function(
   query,
   savepath = getwd(),
-  billing_project_id) {
+  billing_project_id,
+  page_size = 500) {
+
+  if(!rlang::is_character(query)) rlang::abort("`query` argument must contain valid SQL text")
 
 
-
-
-
-
-
-
+  bigrquery::bq_project_query(
+      get_billing_id(),
+      query = query) %>%
+    bigrquery::bq_table_download(page_size = page_size, bigint = "integer64") %>%
+    readr::write_csv(path = savepath)
 
 }
+
+
+
+
+
+read_sql <- function(
+  query,
+  savepath = getwd(),
+  billing_project_id,
+  page_size = 500) {
+
+
+  if(!rlang::is_character(query)) rlang::abort("`query` argument must contain valid SQL text")
+
+  bigrquery::bq_project_query(
+    get_billing_id(),
+    query = query) %>%
+    bigrquery::bq_table_download(page_size = page_size, bigint = "integer64")
+
+}
+
+
