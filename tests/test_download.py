6addd06c640ba5742bd234e7958acf1dec3f4299
Caio R S dos Santos <caiorogerio.santos@gmail.com>
2021-02-01 20:01:11 -0300

    Add metadata functions to download.py module (#227)
    * Add metadata functions to download.py module
    
    * Cleaned up code in some of the functions
    
    * Added docstrings and formatting
    
    * Fixed docstrings for clarity
    
    * Fixed docstring for get_table_size
    
    * Fixed docstrings
    
    * "Functions no longer return a DataFrame with the requested information,
    printing the information directly on screen instead. See coments on the
    get_*_description functions."
    
    * Added **tabulate_kwargs for more control over how tabulate prints the requested info
    
    * Dropped tabulate as the printing method, list functions now fetch short descriptions
    
    * simplify print and description functions
    
    * add some example tests
    
    * bump version
    
    * pretty print results
    
    * update docstring
    
    * bump version
    
    * fixed download tests
    
    * adiciona mais testes das novas funções
    
    * add conditional to shutil.rmtree
    
    * fixed shutil error, add remaining tests
    
    Co-authored-by: Joao Carabetta <joao.carabetta@gmail.com>

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,14 +1,26 @@
 import pytest
 from pathlib import Path
 import pandas as pd
+import shutil
 
-from basedosdados import download, read_sql, read_table
+from basedosdados import (
+    download,
+    read_sql,
+    read_table,
+    list_datasets,
+    list_dataset_tables,
+    get_dataset_description,
+    get_table_description,
+    get_table_columns,
+    get_table_size,
+)
 from basedosdados.exceptions import BaseDosDadosException
 
 
-TEST_PROJECT_ID = "basedosdados-test"
+TEST_PROJECT_ID = "basedosdados-dev"
 SAVEFILE = Path("tests/tmp_bases/test.csv")
 SAVEPATH = Path("tests/tmp_bases/")
+shutil.rmtree(SAVEPATH, ignore_errors=True)
 
 
 def test_download_by_query():
@@ -61,7 +73,7 @@ def test_download_save_to_path():
         limit=10,
     )
 
-    assert (SAVEPATH / "query_result.csv").exists()
+    assert (SAVEPATH / "municipios.csv").exists()
 
 
 def test_download_no_query_or_table():
@@ -110,3 +122,86 @@ def test_read_table():
         ),
         pd.DataFrame,
     )
+
+
+def test_list_datasets(capsys):
+
+    list_datasets()
+    out, err = capsys.readouterr()  # Capture prints
+    assert "dataset_id" in out
+
+
+def test_list_datasets_complete(capsys):
+
+    list_datasets(with_description=True, filter_by="ibge")
+    out, err = capsys.readouterr()  # Capture prints
+    assert "dataset_id" in out
+    assert "description" in out
+
+
+def test_list_datasets_all_descriptions(capsys):
+
+    list_datasets(with_description=True)
+    out, err = capsys.readouterr()  # Capture prints
+    assert len(out) > 0
+
+
+def test_list_dataset_tables(capsys):
+
+    list_dataset_tables(dataset_id="br_ibge_censo2010")
+    out, err = capsys.readouterr()  # Capture prints
+    assert "table_id" in out
+
+
+def test_list_dataset_tables_complete(capsys):
+
+    list_dataset_tables(
+        dataset_id="br_ibge_censo2010",
+        filter_by="renda",
+        with_description=True,
+    )
+    out, err = capsys.readouterr()  # Capture prints
+    assert "table_id" in out
+    assert "description" in out
+    assert "renda" in out
+
+
+def test_list_dataset_tables_all_descriptions(capsys):
+    list_dataset_tables(dataset_id="br_ibge_censo2010", with_description=True)
+    out, err = capsys.readouterr()  # Capture prints
+    assert len(out) > 0
+
+
+def test_get_dataset_description(capsys):
+
+    get_dataset_description("br_ibge_censo2010")
+    out, err = capsys.readouterr()  # Capture prints
+    assert len(out) > 0
+
+
+def test_get_table_description(capsys):
+    get_table_description("br_ibge_censo2010", "pessoa_renda_setor_censitario")
+    out, err = capsys.readouterr()  # Capture prints
+    assert len(out) > 0
+
+
+def test_get_table_columns(capsys):
+    get_table_columns(
+        dataset_id="br_ibge_censo2010",
+        table_id="pessoa_renda_setor_censitario",
+    )
+    out, err = capsys.readouterr()  # Capture prints
+    assert "name" in out
+    assert "field_type" in out
+    assert "description" in out
+
+
+def test_get_table_size(capsys):
+    get_table_size(
+        dataset_id="br_ibge_censo2010",
+        table_id="pessoa_renda_setor_censitario",
+        billing_project_id=TEST_PROJECT_ID,
+    )
+    out, err = capsys.readouterr()
+    assert "num_rows" in out
+    assert "size_mb" in out
