c20c27d217f18a77be6c871ea9cda81a5056dae1
Hellcassius <caiorogerio.santos@gmail.com>
2021-02-10 04:34:31 -0300

    testing actions script

--- .github/workflows/table-approve/main.py
+++ .github/workflows/table-approve/main.py
@@ -1,4 +1,4 @@
-import os
+cdimport os
 import shutil
 from pathlib import Path
 import base64
@@ -146,6 +146,19 @@ def is_partitioned(table_config):
     return table_config["partitions"] is not None
 
 
+def get_table_dataset_id():
+    changes = json.load(Path("/github/workspace/files.json").open("r"))
+    dict_id = {}
+    for change_file in changes:
+        if "table_config.yaml" in change_file:
+            keys = yaml.load(open(change_file, "r"), Loader=yaml.SafeLoader)
+            dict_id[keys["table_id"]] = {
+                "dataset_id": keys["dataset_id"],
+                "source_bucket_name": keys["source_bucket_name"],
+            }
+    return dict_id
+
+
 def push_table_to_bq(
     dataset_id,
     table_id,
@@ -225,11 +238,11 @@ def main():
         "metadata_path": "/github/workspace/bases",
         "gcloud-projects": {
             "staging": {
-                "name": "basedosdados-staging",
+                "name": "test13-01",
                 "credentials_path": "/github/home/.basedosdados/credentials/staging.json",
             },
             "prod": {
-                "name": "basedosdados",
+                "name": "test13-01",
                 "credentials_path": "/github/home/.basedosdados/credentials/prod.json",
             },
         },
@@ -237,40 +250,69 @@ def main():
 
     # prod_base64 = os.environ.get("INPUT_PROD_JSON")
     # staging_base64 = os.environ.get("INPUT_STAGING_JSON")
-    prod_base64 = "'eyJwcm9kX2pzb25fdGVzdCI6InByb2RfanNvbl90ZXN0In0='"
-    staging_base64 = "eyJzdGFnaW5nX2pzb25fdGVzdCI6InN0YWdpbmdfanNvbl90ZXN0In0="
+    prod_base64 = """'ewogICJ0eXBlIjogInNlcnZpY2VfYWNjb3VudCIsCiAgInByb2plY3RfaWQiOiAidGVzdDEzLTAx
+IiwKICAicHJpdmF0ZV9rZXlfaWQiOiAiODZhOGM4MmYwMWZhNTgzOTYzNTU3MzFkZmYxMTI4NWEz
+MWQ3ZDllZSIsCiAgInByaXZhdGVfa2V5IjogIi0tLS0tQkVHSU4gUFJJVkFURSBLRVktLS0tLVxu
+TUlJRXZBSUJBREFOQmdrcWhraUc5dzBCQVFFRkFBU0NCS1l3Z2dTaUFnRUFBb0lCQVFDNjNhMlJa
+VUx0bGw2b1xuT0QyQ0lhKzNDaVBadDhicUxpZG81RHcyV3VDOHo4UmJTWlNDM3RlUTVlQW1IMTNI
+TzVtYTZ3QkxHYXYyNWNNN1xuenFsRVphdERnY3I4S2xXTUhnZGdYRjNwU0JDTEdlbEpEVS82NW9U
+SXZuYnlTU3h4cFliUFdzWWRIZGNjbEhFQ1xuSXdqNEREMmFneWJmT2M4M1BKQWYzZkM1T1lUbXBL
+dUl4T0owTEdxaVlVdkFZY2k4QTlqVWRUalhvbUtlMzlyZFxuM1hySGtJWHZoWHM1cklRTEozVVJZ
+NXJwRklSYnV5MWJ0UHpndHd2eUZWQWo1K3RJMk1PUDUraHA4ODEyTHlScVxuUkhmTUxOYlJteGhy
+YkF3Zk1ycW1qSS9sRytucndWY1dFcGNEVFpCS0xjdDc5RzYra2QwWFNxNzB0Mk5YeUdka1xuQ1Ix
+ODlSbnpBZ01CQUFFQ2dnRUFDUVVTcVlMK3gyL2xTV0tiK2tCRE5uUURjcGQvdnhqVG9QWHJXVk9F
+ZEY4WVxubkxBNnZsUUc4Q0Q0YzNIVWpNcGdTZTF2L1RBSENmRklDcUJtRjZRWDR0Rk0xbVJndTRp
+QVZYTC83a084Y0xEV1xuZnVCcEJvR1BPSFZoSG5JRHcyRmZDWHJabmFYSFNpbXNJZm1zSjF0aGdF
+R2N5b2VlQmZSV09XbjA2TjhlR0NTd1xuWHl6cEUxZ0VVRnF5a0lkNDBQNUZaeFpkZWJwQlZTTFl3
+OUk0TmNhUjQyZElSUVBVRUFoN1ZhckxvNjVIdzVVWFxuMzgwb21JWWlGbXJxV2lrbXBrYWFWTUpB
+emYxUS9qcTRHdGxtYmtPVERuMmQ0VC9abHpTRHBUaXJCN08vdHAzdFxuVmV0V0xtYm5tSzlRd3lB
+Y1MyZENSaG9CNEJ5SG04SzN3NUdMY0s1ditRS0JnUURlY3RHN1ROZEQvSFJwM3h2TVxuT0RHcFpK
+RjNianVXSXFoTU5CWDUyUlZIcS9hZ1Z1SlBzZnlBMTN3S1Y4TzRTM1F5aUl1ZjJvVVNhNEg1MUI3
+WlxuLzJYTTZ3Lys5OGdJdWN3eitaR05rdGU5TlkxaitWeTMwaHRtdTBRaXFVTm9LVTN5b1ZsR3g0
+SUUyWkdFUmhHSFxuUSt3amx4Q25BcThMeElBc3FVK21uNENJOXdLQmdRRFhEUEc1ZXBqNlpuNnpY
+SjRCWmNZOXN4T2lWQ3diVHJURlxuK25yRWVQSmNTaGxreXBuelM1dng1bEJuUGFsSERKaytnTnVM
+cWJ4Q2FVZTlLWE5oOTdHZGtHdEh4UGJZTmdJd1xubmVVNlhoSjlEOVgvZ2J1VmtqM2NTSzJVV3ds
+QlZGTHR6SklqSlpVSWlSYm9zOGpVRUYwTUZKOFh2Nm9CYWlWQ1xuYlRCZ0srSFQ1UUtCZ0NvNWNV
+WTBWOGczNjRFTk1LR2JLUklXWE9abXJqalphMGpMdWtBcXpMZEdGUkpxYTVybFxuRmlEK3hqVFFZ
+LzVmbERialpGMTdoVGJ4NFVJaTJaaFh1bU1qVzIzeWFxMzlWcDJuQ2RIdHhiWE1ySUlGbTJ2cFxu
+SGZwcUlZelN4RkRKUmxwLzlncFJaSVMzSjhBTDdOZllOTUtzc2lTQmhlaWQ3QmFPTE9oYjduSVJB
+b0dBYmhZZFxuUkJMamJ3TzdCbng2RWtNVVdZOXJsS1M4TTNwMkJnYzBnNUZhbUM0Q2s5czNOaG5W
+aWQ2WitFL1RjU0NjN2ljdVxudkc4MmhWUzV1YmNYYnVaS2tWdFYzOWFQZXAwalA5b0VkMzJpdjB5
+MUF1aFpxN2JDWGhzQ2FMaTFvekRVaHhFN1xuR2l6cC93V1dxYWNuUG8vbDRnSkljdkxWeDRXYjcw
+WlFCbHRRYnBVQ2dZQVQwV2hxeTUrejR6NWtkUEh4ZkxpY1xuZ2gvZm53OFJLR0FoTmNubjZXdGFB
+MzVWRXZCaHJPRGJxK1g3MWlKSXpqYk5sa2ZqRzU0MjlFcmJQTmhaTzB4Z1xuT3NWSmZiM2Z5SmRO
+YTRvNFdsOThJTFVPQlRUMjBFNHJtd09mcmUxamh1QzM2YXlOYldQTi9vWEhGNWxKUU0zclxubFlQ
+dFk5VGFOWHR5ckdmZi91WWJYdz09XG4tLS0tLUVORCBQUklWQVRFIEtFWS0tLS0tXG4iLAogICJj
+bGllbnRfZW1haWwiOiAiY2Fpby04MTdAdGVzdDEzLTAxLmlhbS5nc2VydmljZWFjY291bnQuY29t
+IiwKICAiY2xpZW50X2lkIjogIjEwMTIzMTk3ODk0OTEzNDk2ODExOSIsCiAgImF1dGhfdXJpIjog
+Imh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbS9vL29hdXRoMi9hdXRoIiwKICAidG9rZW5fdXJp
+IjogImh0dHBzOi8vb2F1dGgyLmdvb2dsZWFwaXMuY29tL3Rva2VuIiwKICAiYXV0aF9wcm92aWRl
+cl94NTA5X2NlcnRfdXJsIjogImh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMuY29tL29hdXRoMi92MS9j
+ZXJ0cyIsCiAgImNsaWVudF94NTA5X2NlcnRfdXJsIjogImh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMu
+Y29tL3JvYm90L3YxL21ldGFkYXRhL3g1MDkvY2Fpby04MTclNDB0ZXN0MTMtMDEuaWFtLmdzZXJ2
+aWNlYWNjb3VudC5jb20iCn0K'"""
+    staging_base64 = prod_base64
 
     create_config_folder(".basedosdados")
     create_json_file(prod_base64, "prod.json", ".basedosdados")
     create_json_file(staging_base64, "staging.json", ".basedosdados")
     save_toml(config_dict, "config.toml", ".basedosdados")
 
-    changes = json.load(Path("/github/workspace/files.json").open("r"))
-    dict_id = {}
-    for change_file in changes:
-        if "table_config.yaml" in change_file:
-            keys = yaml.load(open(change_file, "r"), Loader=yaml.SafeLoader)
-            dict_id[keys["table_id"]] = {
-                "dataset_id": keys["dataset_id"],
-                "source_bucket_name": keys["source_bucket_name"],
-            }
-    print(dict_id)
+    dict_id = get_table_dataset_id()
 
-    # dataset_id = ????
-    # table_id = ????
     for table_id in dict_id.keys():
         source_bucket_name = dict_id[table_id]["source_bucket_name"]
         dataset_id = dict_id[table_id]["dataset_id"]
         print(source_bucket_name, dataset_id)
-        # push_table_to_bq(
-        #     dataset_id=dataset_id,
-        #     table_id=table_id,
-        #     source_bucket_name=source_bucket_name,
-        #     destination_bucket_name="basedosdados-sv",
-        #     backup_bucket_name="basedosdados-sv-bkp",
-        # )
+        push_table_to_bq(
+            dataset_id=dataset_id,
+            table_id=table_id,
+            source_bucket_name=source_bucket_name,
+            destination_bucket_name="test-13-01-bucket",
+            backup_bucket_name="test-13-01-backup",
+        )
 
-    "/github/workspace/files.json"
+    # "/github/workspace/files.json"
 
     check_function()
 
