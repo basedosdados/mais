7ece1c2db00fb2ad704a314268bb09569ba68643
João Carabetta <joao.carabetta@gmail.com>
2021-02-12 20:36:11 -0300

    Revert "Revert "[dados] br_tse_eleicoes.despesas_candidato (#270)" (#271)"
    This reverts commit a2dd54daf880cbf0a6bdbe0a9ea6bd9a0cd8ebdd.

--- bases/br_tse_eleicoes/receita_candidato/publish.sql
+++ bases/br_tse_eleicoes/receita_candidato/publish.sql
@@ -18,7 +18,8 @@ TIPOS:
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
 
 */
-CREATE VIEW basedosdados-dev.br_tse_eleicoes.receitas_candidato AS
+
+CREATE VIEW basedosdados.br_tse_eleicoes.receita_candidato AS
 SELECT 
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(tipo_eleicao AS STRING) tipo_eleicao,
@@ -39,11 +40,12 @@ SAFE_CAST(sigla_partido AS STRING) sigla_partido,
 SAFE_CAST(cargo AS STRING) cargo,
 SAFE_CAST(sequencial_receita AS INT64) sequencial_receita,
 SAFE_CAST(data_receita AS DATE) data_receita,
+SAFE_CAST(tipo_receita AS STRING) tipo_receita,
+SAFE_CAST(tipo_fonte_receita AS STRING) tipo_fonte_receita,
 SAFE_CAST(fonte_receita AS STRING) fonte_receita,
+SAFE_CAST(situacao_receita AS STRING) situacao_receita,
 SAFE_CAST(origem_receita AS STRING) origem_receita,
 SAFE_CAST(natureza_receita AS STRING) natureza_receita,
-SAFE_CAST(especie_receita AS STRING) especie_receita,
-SAFE_CAST(situacao_receita AS STRING) situacao_receita,
 SAFE_CAST(descricao_receita AS STRING) descricao_receita,
 SAFE_CAST(valor_receita AS FLOAT64) valor_receita,
 SAFE_CAST(sequencial_candidato_doador AS INT64) sequencial_candidato_doador,
@@ -58,13 +60,13 @@ SAFE_CAST(sigla_partido_doador AS STRING) sigla_partido_doador,
 SAFE_CAST(nome_partido_doador AS STRING) nome_partido_doador,
 SAFE_CAST(esfera_partidaria_doador AS STRING) esfera_partidaria_doador,
 SAFE_CAST(numero_candidato_doador AS INT64) numero_candidato_doador,
-SAFE_CAST(cnae_2_doador AS STRING) cnae_2_doador,
-SAFE_CAST(descricao_cnae_2_doador AS STRING) descricao_cnae_2_doador,
+SAFE_CAST(id_setor_economico_doador AS STRING) id_setor_economico_doador,
+SAFE_CAST(setor_economico_doador AS STRING) setor_economico_doador,
 SAFE_CAST(cpf_cnpj_doador_orig AS STRING) cpf_cnpj_doador_orig,
 SAFE_CAST(nome_doador_orig AS STRING) nome_doador_orig,
 SAFE_CAST(nome_doador_orig_rf AS STRING) nome_doador_orig_rf,
 SAFE_CAST(tipo_doador_orig AS STRING) tipo_doador_orig,
-SAFE_CAST(descricao_cnae_2_doador_orig AS STRING) descricao_cnae_2_doador_orig,
+SAFE_CAST(setor_economico_doador_orig AS STRING) setor_economico_doador_orig,
 SAFE_CAST(nome_administrador AS STRING) nome_administrador,
 SAFE_CAST(cpf_administrador AS STRING) cpf_administrador,
 SAFE_CAST(numero_recibo_eleitoral AS STRING) numero_recibo_eleitoral,
@@ -72,8 +74,8 @@ SAFE_CAST(numero_documento AS STRING) numero_documento,
 SAFE_CAST(numero_recibo_doacao AS STRING) numero_recibo_doacao,
 SAFE_CAST(numero_documento_doacao AS STRING) numero_documento_doacao,
 SAFE_CAST(tipo_prestacao_contas AS STRING) tipo_prestacao_contas,
-SAFE_CAST(data_prestacao_contas AS DATE) data_prestacao_contas,
-SAFE_CAST(sequencial_prestador_contas AS INT64) sequencial_prestador_contas,
+SAFE_CAST(data_prestacao_contas AS STRING) data_prestacao_contas,
+SAFE_CAST(sequencial_prestador_contas AS STRING) sequencial_prestador_contas,
 SAFE_CAST(cnpj_prestador_contas AS STRING) cnpj_prestador_contas,
 SAFE_CAST(entrega_conjunto AS STRING) entrega_conjunto
-from basedosdados-dev.br_tse_eleicoes_staging.receitas_candidato as t
+from basedosdados-staging.br_tse_eleicoes_staging.receita_candidato as t
