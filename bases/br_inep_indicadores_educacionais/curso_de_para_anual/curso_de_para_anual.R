6606cb35382b5ce9962a9ab9231c839bed21b95a
Hevilyn <hevilynsouza12@hotmail.com>
2021-02-18 18:17:51 -0300

    Tabela: INEP - Curso

--- bases/br_inep_indicadores_educacionais/curso_de_para_anual/curso_de_para_anual.R
+++ bases/br_inep_indicadores_educacionais/curso_de_para_anual/curso_de_para_anual.R
@@ -6,7 +6,7 @@ library(xlsx)
 
 setwd("C:/Users/Hevilyn/Desktop/inep_curso/")
 
-tjes_15_19 <- read.csv2("curso_de_para_anual.csv")
+tjes_15_19 <- ead_excel("indicadores_trajetoria_educacao_superior_2014_2019.xlsx")
 tjes_15_19 <- tjes_15_19[-c(1:8,140704),]
 
 tjes_15_19 <- as.data.frame(tjes_15_19)
@@ -23,18 +23,6 @@ colnames(tjes_15_19) <- c("id_ies","nome_ies","tipo_categoria_administrativa","t
                 ,"prazo_integralizacao","ano_integralizacao","prazo_acompanhamento","ano_max_acompanhamento"
                 ,"quantidade_ingressante","quantidade_permanencia","quantidade_concluinte","quantidade_desistencia","quantidade_falecido","tap","tca"
                 ,"tda","tcan","tada")
-
-tjes_15_19$nome_ies <- arrumar(tjes_15_19$nome_ies)
-tjes_15_19$nome_ies <- arrumar_cons(tjes_15_19$nome_ies)
-tjes_15_19$nome_curso <- arrumar(tjes_15_19$nome_curso)
-tjes_15_19$nome_curso <- arrumar_cons(tjes_15_19$nome_curso)
-tjes_15_19$nome_cine_area_geral <- arrumar(tjes_15_19$nome_cine_area_geral)
-tjes_15_19$nome_cine_area_geral <- arrumar_cons(tjes_15_19$nome_cine_area_geral)
-tjes_15_19$nome_cine_rotulo <- arrumar(tjes_15_19$nome_cine_rotulo)
-tjes_15_19$nome_cine_rotulo <- arrumar_cons(tjes_15_19$nome_cine_rotulo)
-
-tjes_15_19[25:31] <- options(digits = 2)
-
 
 tjes_14_19 <- read_excel("indicadores_trajetoria_educacao_superior_2014_2019.xlsx")
 tjes_14_19 <- tjes_14_19[-c(1:8,164361),]
@@ -69,7 +57,6 @@ tjes_01 <- rbind(tjes_15_19,tjes)
 
 tjes_01 <- arrumar(tjes_01)
 tjes_01<- arrumar_cons(tjes_01)
-
 
 tjes_01$tada <- as.numeric(tjes_01$tada)
 tjes_01$tap <- as.numeric(tjes_01$tap)
