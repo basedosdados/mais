35930ce5268dd5725ae13e5675d0a915060a9f32
hellcassius <caiorogerio.santos@gmail.com>
2021-03-11 03:28:39 -0300

    fix error instances on table.init

--- basedosdados/table.py
+++ basedosdados/table.py
@@ -156,9 +156,12 @@ class Table(Base):
             elif if_folder_exists == "pass":
                 return self
 
-        # Raise: without a path to data sample, should only work if config already exists
+        # Raise: without a path to data sample, should not replace config files with empty template
         if not data_sample_path:
-            if if_table_config_exists != "pass":
+            if (
+                if_table_config_exists == "replace"
+                and Path(self.table_folder / "table_config.yaml").is_file()
+            ):
                 raise BaseDosDadosException(
                     "You must provide a path to correctly create config files"
                 )
@@ -221,6 +224,7 @@ class Table(Base):
         else:
             if if_table_config_exists == "raise":
 
+                # Check if config files already exist
                 if (
                     Path(self.table_folder / "table_config.yaml").is_file()
                     and Path(self.table_folder / "publish.sql").is_file()
@@ -229,9 +233,8 @@ class Table(Base):
                     raise FileExistsError(
                         f"table_config.yaml and publish.sql already exists at {self.table_folder}"
                     )
-
+                # if config files don't exist, create them
                 else:
-
                     self._make_template(columns, partition_columns)
 
             if if_table_config_exists == "replace":
