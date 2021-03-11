1278c3f65ebfc05f6781facaf3139d0ce7011388
Ricardo Dahis <rdahis@gmail.com>
2021-03-11 10:25:49 -0800

    Sobe tabelas.

---
+++ bases/br_me_comex_stat/municipio_exportacao/publish.sql
@@ -0,0 +1,32 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+
+CREATE VIEW basedosdados-dev.br_me_comex_stat.municipio_exportacao AS
+SELECT 
+SAFE_CAST(id_sh4 AS STRING) id_sh4,
+SAFE_CAST(id_pais AS STRING) id_pais,
+SAFE_CAST(sigla_uf AS STRING) sigla_uf,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
+SAFE_CAST(peso_liquido_kg AS STRING) peso_liquido_kg,
+SAFE_CAST(valor_fob_dolar AS STRING) valor_fob_dolar,
+SAFE_CAST(ano AS STRING) ano,
+SAFE_CAST(mes AS STRING) mes
+from basedosdados-dev.br_me_comex_stat_staging.municipio_exportacao as t
---
+++ bases/br_me_comex_stat/ncm_exportacao/publish.sql
@@ -0,0 +1,35 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+
+CREATE VIEW basedosdados-dev.br_me_comex_stat.ncm_exportacao AS
+SELECT 
+SAFE_CAST(id_ncm AS STRING) id_ncm,
+SAFE_CAST(id_unidade AS STRING) id_unidade,
+SAFE_CAST(id_pais AS STRING) id_pais,
+SAFE_CAST(sigla_uf_ncm AS STRING) sigla_uf_ncm,
+SAFE_CAST(id_via AS STRING) id_via,
+SAFE_CAST(id_urf AS STRING) id_urf,
+SAFE_CAST(quantidade_estatistica AS STRING) quantidade_estatistica,
+SAFE_CAST(peso_liquido_kg AS STRING) peso_liquido_kg,
+SAFE_CAST(valor_fob_dolar AS STRING) valor_fob_dolar,
+SAFE_CAST(ano AS STRING) ano,
+SAFE_CAST(mes AS STRING) mes
+from basedosdados-dev.br_me_comex_stat_staging.ncm_exportacao as t
