2a2a833f07696e9fbb33d8e1f812c6db1f23716a
d116626 <d116626@gmail.com>
2021-03-12 13:02:58 -0300

    [dados] br_sp_gov_ssp | padronizacao colunas

--- bases/br_sp_gov_ssp/ocorrecencias_registradas/publish.sql
+++ bases/br_sp_gov_ssp/ocorrecencias_registradas/publish.sql
@@ -13,22 +13,23 @@ Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
 # para algo um pouco mais explícito.
 
 TIPOS:
-    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Para modificar tipos de colunas, basta substituir INT64 por outro tipo válido.
     - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
 
 */
-CREATE VIEW basedosdados.br_sp_ssp_seguranca.ocorrecencias_registradas AS
+
+CREATE VIEW basedosdados-dev.br_sp_gov_ssp.ocorrecencias_registradas AS
 SELECT 
-SAFE_CAST(id_municipio AS INT64) id_municipio,
-SAFE_CAST(id_estado AS INT64) id_estado,
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(id_municipio AS INT64) id_municipio,
+SAFE_CAST(id_estado AS INT64) id_estado,
 SAFE_CAST(regiao_ssp AS STRING) regiao_ssp,
 SAFE_CAST(homicidio_doloso AS INT64) homicidio_doloso,
-SAFE_CAST(no_de_vitimas_em_homicidio_doloso AS INT64) no_de_vitimas_em_homicidio_doloso,
+SAFE_CAST(numero_de_vitimas_em_homicidio_doloso AS INT64) numero_de_vitimas_em_homicidio_doloso,
 SAFE_CAST(homicidio_doloso_por_acidente_de_transito AS INT64) homicidio_doloso_por_acidente_de_transito,
-SAFE_CAST(no_de_vitimas_em_homicidio_doloso_por_acidente_de_transito AS INT64) no_de_vitimas_em_homicidio_doloso_por_acidente_de_transito,
+SAFE_CAST(numero_de_vitimas_em_homicidio_doloso_por_acidente_de_transito AS INT64) numero_de_vitimas_em_homicidio_doloso_por_acidente_de_transito,
 SAFE_CAST(homicidio_culposo_por_acidente_de_transito AS INT64) homicidio_culposo_por_acidente_de_transito,
 SAFE_CAST(homicidio_culposo_outros AS INT64) homicidio_culposo_outros,
 SAFE_CAST(tentativa_de_homicidio AS INT64) tentativa_de_homicidio,
@@ -37,7 +38,7 @@ SAFE_CAST(lesao_corporal_dolosa AS INT64) lesao_corporal_dolosa,
 SAFE_CAST(lesao_corporal_culposa_por_acidente_de_transito AS INT64) lesao_corporal_culposa_por_acidente_de_transito,
 SAFE_CAST(lesao_corporal_culposa_outras AS INT64) lesao_corporal_culposa_outras,
 SAFE_CAST(latrocinio AS INT64) latrocinio,
-SAFE_CAST(no_de_vitimas_em_latrocinio AS INT64) no_de_vitimas_em_latrocinio,
+SAFE_CAST(numero_de_vitimas_em_latrocinio AS INT64) numero_de_vitimas_em_latrocinio,
 SAFE_CAST(total_de_estupro AS INT64) total_de_estupro,
 SAFE_CAST(estupro AS INT64) estupro,
 SAFE_CAST(estupro_de_vulneravel AS INT64) estupro_de_vulneravel,
@@ -48,4 +49,4 @@ SAFE_CAST(roubo_a_banco AS INT64) roubo_a_banco,
 SAFE_CAST(roubo_de_carga AS INT64) roubo_de_carga,
 SAFE_CAST(furto_outros AS INT64) furto_outros,
 SAFE_CAST(furto_de_veiculo AS INT64) furto_de_veiculo
-from basedosdados-staging.br_sp_gov_ssp_staging.ocorrecencias_registradas as t
+from basedosdados-dev.br_sp_gov_ssp_staging.ocorrecencias_registradas as t
--- bases/br_sp_gov_ssp/produtividade_policial/publish.sql
+++ bases/br_sp_gov_ssp/produtividade_policial/publish.sql
@@ -13,29 +13,30 @@ Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
 # para algo um pouco mais explícito.
 
 TIPOS:
-    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Para modificar tipos de colunas, basta substituir INT64 por outro tipo válido.
     - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
 
 */
-CREATE VIEW basedosdados.br_sp_ssp_seguranca.produtividade_policial AS
-SELECT 
-SAFE_CAST(id_municipio AS INT64) id_municipio,
-SAFE_CAST(id_estado AS INT64) id_estado,
+
+CREATE VIEW basedosdados-dev.br_sp_gov_ssp.produtividade_policial AS
+SELECT
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(id_municipio AS INT64) id_municipio,
+SAFE_CAST(id_estado AS INT64) id_estado,
 SAFE_CAST(regiao_ssp AS STRING) regiao_ssp,
 SAFE_CAST(ocorrencias_de_porte_de_entorpecentes AS INT64) ocorrencias_de_porte_de_entorpecentes,
 SAFE_CAST(ocorrencias_de_trafico_de_entorpecentes AS INT64) ocorrencias_de_trafico_de_entorpecentes,
 SAFE_CAST(ocorrencias_de_apreensao_de_entorpecentes AS INT64) ocorrencias_de_apreensao_de_entorpecentes,
 SAFE_CAST(ocorrencias_de_porte_ilegal_de_arma AS INT64) ocorrencias_de_porte_ilegal_de_arma,
-SAFE_CAST(no_de_armas_de_fogo_apreendidas AS INT64) no_de_armas_de_fogo_apreendidas,
-SAFE_CAST(no_de_flagrantes_lavrados AS INT64) no_de_flagrantes_lavrados,
-SAFE_CAST(no_de_infratores_apreendidos_em_flagrante AS INT64) no_de_infratores_apreendidos_em_flagrante,
-SAFE_CAST(no_de_infratores_apreendidos_por_mandado AS INT64) no_de_infratores_apreendidos_por_mandado,
-SAFE_CAST(no_de_pessoas_presas_em_flagrante AS INT64) no_de_pessoas_presas_em_flagrante,
-SAFE_CAST(no_de_pessoas_presas_por_mandado AS INT64) no_de_pessoas_presas_por_mandado,
-SAFE_CAST(no_de_prisoes_efetuadas AS INT64) no_de_prisoes_efetuadas,
-SAFE_CAST(no_de_veiculos_recuperados AS INT64) no_de_veiculos_recuperados,
-SAFE_CAST(tot_de_inqueritos_policiais_instaurados AS INT64) tot_de_inqueritos_policiais_instaurados
-from basedosdados-staging.br_sp_gov_ssp_staging.produtividade_policial as t
+SAFE_CAST(numero_de_armas_de_fogo_apreendidas AS INT64) numero_de_armas_de_fogo_apreendidas,
+SAFE_CAST(numero_de_flagrantes_lavrados AS INT64) numero_de_flagrantes_lavrados,
+SAFE_CAST(numero_de_infratores_apreendidos_em_flagrante AS INT64) numero_de_infratores_apreendidos_em_flagrante,
+SAFE_CAST(numero_de_infratores_apreendidos_por_mandado AS INT64) numero_de_infratores_apreendidos_por_mandado,
+SAFE_CAST(numero_de_pessoas_presas_em_flagrante AS INT64) numero_de_pessoas_presas_em_flagrante,
+SAFE_CAST(numero_de_pessoas_presas_por_mandado AS INT64) numero_de_pessoas_presas_por_mandado,
+SAFE_CAST(numero_de_prisoes_efetuadas AS INT64) numero_de_prisoes_efetuadas,
+SAFE_CAST(numero_de_veiculos_recuperados AS INT64) numero_de_veiculos_recuperados,
+SAFE_CAST(total_de_inqueritos_policiais_instaurados AS INT64) total_de_inqueritos_policiais_instaurados
+from basedosdados-dev.br_sp_gov_ssp_staging.produtividade_policial as t
