c90ffe6adc5b721d5b5be2d4d33f32890c91e1eb
Matheus Valentim <75315649+mavalentim@users.noreply.github.com>
2021-03-24 01:25:20 -0300

    [dados] br_sp_seduc_fluxo_escolar (#338)
    * colocando primeira base do fluxo escolar
    
    * Adicionando base a nivel de escola
    
    * Corrigindo os erros apontados
    
    * Corrigindo e harmonizando bases
    
    * ajustando a tabela escola

---
+++ bases/br_sp_seduc_fluxo_escolar/municipio/publish.sql
@@ -0,0 +1,38 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+
+CREATE VIEW basedosdados-dev.br_sp_seduc_fluxo_escolar.municipio AS
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(sigla_uf AS STRING) sigla_uf,
+SAFE_CAST(rede AS STRING) rede,
+SAFE_CAST(diretoria AS STRING) diretoria,
+SAFE_CAST(id_municipio AS INT64) id_municipio,
+SAFE_CAST(prop_aprovados_anos_inciais_ef AS FLOAT64) prop_aprovados_anos_inciais_ef,
+SAFE_CAST(prop_reprovados_anos_iniciais_ef AS FLOAT64) prop_reprovados_anos_iniciais_ef,
+SAFE_CAST(prop_abandono_anos_iniciais_ef AS FLOAT64) prop_abandono_anos_iniciais_ef,
+SAFE_CAST(prop_aprovados_anos_finais_ef AS FLOAT64) prop_aprovados_anos_finais_ef,
+SAFE_CAST(prop_reprovados_anos_finais_ef AS FLOAT64) prop_reprovados_anos_finais_ef,
+SAFE_CAST(prop_abandono_anos_finais_ef AS FLOAT64) prop_abandono_anos_finais_ef,
+SAFE_CAST(prop_aprovados_em AS FLOAT64) prop_aprovados_em,
+SAFE_CAST(prop_reprovados_em AS FLOAT64) prop_reprovados_em,
+SAFE_CAST(prop_abandono_em AS FLOAT64) prop_abandono_em
+from basedosdados-dev.br_sp_seduc_fluxo_escolar_staging.municipio as t
