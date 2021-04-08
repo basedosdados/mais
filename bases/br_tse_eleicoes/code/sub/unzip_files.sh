b77addc3c374cd5af3b4e9d6a9340c2673f85684
Ricardo Dahis <rdahis@gmail.com>
2021-04-07 23:02:45 -0700

    Atualiza código e conserta erro da variável resultado para presidente.

--- bases/br_tse_eleicoes/code/sub/unzip_files.sh
+++
@@ -1,7 +0,0 @@
-
-#cd "/Users/ricardodahis/Downloads/dados_TSE/input/filiacao_partidaria"
-
-cd "/Users/ricardodahis/Downloads/dados_TSE/input/votacao_secao"
-for i in *.zip; do
-    unp -u "$i"
-done
