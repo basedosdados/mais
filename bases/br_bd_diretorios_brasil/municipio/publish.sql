a51b928ebcf16239b1e9f162f3e59a0a1ce54d66
Ricardo Dahis <rdahis@gmail.com>
2021-05-02 13:16:27 -0700

    Atualiza o diretório de municípios.

--- bases/br_bd_diretorios_brasil/municipio/publish.sql
+++ bases/br_bd_diretorios_brasil/municipio/publish.sql
@@ -18,32 +18,28 @@ TIPOS:
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
 
 */
-
-CREATE VIEW basedosdados.br_bd_diretorios_brasil.municipio AS
+CREATE VIEW basedosdados-dev.br_bd_diretorios_brasil.municipio AS
 SELECT 
-SAFE_CAST(id_municipio AS INT64) id_municipio,
-SAFE_CAST(id_municipio_6 AS INT64) id_municipio_6,
-SAFE_CAST(id_municipio_tse AS INT64) id_municipio_tse,
-SAFE_CAST(id_municipio_rf AS INT64) id_municipio_rf,
-SAFE_CAST(id_municipio_bcb AS INT64) id_municipio_bcb,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
+SAFE_CAST(id_municipio_6 AS STRING) id_municipio_6,
+SAFE_CAST(id_municipio_tse AS STRING) id_municipio_tse,
+SAFE_CAST(id_municipio_rf AS STRING) id_municipio_rf,
+SAFE_CAST(id_municipio_bcb AS STRING) id_municipio_bcb,
 SAFE_CAST(municipio AS STRING) municipio,
 SAFE_CAST(capital_uf AS INT64) capital_uf,
-SAFE_CAST(id_comarca AS INT64) id_comarca,
-SAFE_CAST(id_regiao_saude AS INT64) id_regiao_saude,
+SAFE_CAST(id_comarca AS STRING) id_comarca,
+SAFE_CAST(id_regiao_saude AS STRING) id_regiao_saude,
 SAFE_CAST(regiao_saude AS STRING) regiao_saude,
-SAFE_CAST(id_regiao_imediata AS INT64) id_regiao_imediata,
+SAFE_CAST(id_regiao_imediata AS STRING) id_regiao_imediata,
 SAFE_CAST(regiao_imediata AS STRING) regiao_imediata,
-SAFE_CAST(id_regiao_intermediaria AS INT64) id_regiao_intermediaria,
+SAFE_CAST(id_regiao_intermediaria AS STRING) id_regiao_intermediaria,
 SAFE_CAST(regiao_intermediaria AS STRING) regiao_intermediaria,
-SAFE_CAST(id_microrregiao AS INT64) id_microrregiao,
+SAFE_CAST(id_microrregiao AS STRING) id_microrregiao,
 SAFE_CAST(microrregiao AS STRING) microrregiao,
-SAFE_CAST(id_mesorregiao AS INT64) id_mesorregiao,
+SAFE_CAST(id_mesorregiao AS STRING) id_mesorregiao,
 SAFE_CAST(mesorregiao AS STRING) mesorregiao,
-SAFE_CAST(id_uf AS INT64) id_uf,
+SAFE_CAST(id_uf AS STRING) id_uf,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(uf AS STRING) uf,
-SAFE_CAST(regiao AS STRING) regiao,
-SAFE_CAST(existia_1991 AS INT64) existia_1991,
-SAFE_CAST(existia_2000 AS INT64) existia_2000,
-SAFE_CAST(existia_2010 AS INT64) existia_2010
-from basedosdados-staging.br_bd_diretorios_brasil_staging.municipio as t
+SAFE_CAST(regiao AS STRING) regiao
+FROM basedosdados-dev.br_bd_diretorios_brasil_staging.municipio AS t
