67776b4a428bac0f38f10a7d0d82566b0cb633b2
d116626 <d116626@gmail.com>
2021-05-15 21:24:52 -0300

    [infra] auto fill table_config.yaml descriptions

--- python-package/basedosdados/upload/table.py
+++ python-package/basedosdados/upload/table.py
@@ -6,6 +6,11 @@ from copy import deepcopy
 from google.cloud import bigquery
 import datetime
 
+import ruamel_yaml
+import requests
+from io import StringIO
+import pandas as pd
+
 import google.api_core.exceptions
 
 from basedosdados.upload.base import Base
@@ -151,12 +156,36 @@ class Table(Base):
                     template
                 )
 
+    def _sheet_to_df(self, columns_config_url):
+        url = columns_config_url.replace("edit#gid=", "export?format=csv&gid=")
+        return pd.read_csv(StringIO(requests.get(url).content.decode("utf-8")))
+
+    def update_columns(self, columns_config_url):
+        """Fills descriptions of tables automatically using a public google sheets URL.
+        The URL must be in the format https://docs.google.com/spreadsheets/d/<table_key>/edit#gid=<table_gid>
+        Args:
+            columns_config_url (str): google sheets URL.
+        """
+        ruamel = ruamel_yaml.YAML()
+        ruamel.preserve_quotes = True
+        with open(self.table_folder / "table_config.yaml") as fp:
+            config_yaml = ruamel.load(fp)
+
+        df = self._sheet_to_df(columns_config_url)
+        columns_parametes = zip(df["coluna"].tolist(), df["descricao"].tolist())
+        for name, description in columns_parametes:
+            for col in config_yaml["columns"]:
+                if col["name"] == name:
+                    col["description"] = description
+        ruamel.dump(config_yaml, stream=self.table_folder / "table_config.yaml")
+
     def init(
         self,
         data_sample_path=None,
         if_folder_exists="raise",
         if_table_config_exists="raise",
         source_format="csv",
+        columns_config_url=None,
     ):
         """Initialize table folder at metadata_path at `metadata_path/<dataset_id>/<table_id>`.
 
@@ -185,6 +214,8 @@ class Table(Base):
                 * 'pass' : Do nothing
             source_format (str): Optional
                 Data source format. Only 'csv' is supported. Defaults to 'csv'.
+
+            columns_config_url (str): google sheets URL.
 
 
         Raises:
@@ -278,6 +309,9 @@ class Table(Base):
             # Raise: without a path to data sample, should not replace config files with empty template
             self._make_template(columns, partition_columns)
 
+        if columns_config_url is not None:
+            self.update_columns(columns_config_url)
+
         return self
 
     def create(
@@ -289,6 +323,7 @@ class Table(Base):
         if_storage_data_exists="raise",
         if_table_config_exists="raise",
         source_format="csv",
+        columns_config_url=None,
     ):
         """Creates BigQuery table at staging dataset.
 
@@ -335,6 +370,8 @@ class Table(Base):
                 * 'pass' : Do nothing
             source_format (str): Optional
                 Data source format. Only 'csv' is supported. Defaults to 'csv'.
+
+            columns_config_url (str): google sheets URL.
         """
 
         if path is None:
@@ -379,6 +416,7 @@ class Table(Base):
             data_sample_path=path,
             if_folder_exists="replace",
             if_table_config_exists=if_table_config_exists,
+            columns_config_url=columns_config_url,
         )
 
         table = bigquery.Table(self.table_full_name["staging"])
