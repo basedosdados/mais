40d828ee80941ef2eda75862415fef439defeb5d
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2021-05-22 20:39:38 -0300

    Update bdplyr.R
    mudanças sugeridas

--- r-package/R/bdplyr.R
+++ r-package/R/bdplyr.R
@@ -1,7 +1,7 @@
 #' Compatibilidade com {dplyr}
 #'
 #' @description
-#'
+#' # TODO: colocar em inglês as docs
 #' Realiza conexão com o Google BigQuery via {DBI} e torna a base
 #' compatível com as operações básicas do {dplyr} tais como glimpse(),
 #' filter(), select(), mutate(), _join(), etc.
@@ -65,33 +65,79 @@
 #'
 #'}
 
-bdplyr <- function(tabela,
-                   billing_project_id = basedosdados::get_billing_id()) {
+bdplyr <- function(
+  table,
+  billing_project_id = basedosdados::get_billing_id()) {
 
   # checar se o billing foi informado
+
   if(billing_project_id == FALSE) {
+
     rlang::abort("O billing_id não foi informado.")
-    return(FALSE)
+
   }
 
   # criar o nome que o BQ vai entender
   # TODO: checagem se o nome é válido
-  tabela_completa <- paste0("basedosdados.", tabela)
+
+  tabela_completa <- glue::glue("basedosdados.{table}")
 
   # checa se a tabela é reconhecida pelo google
+
   if(!bigrquery::bq_table_exists(tabela_completa)) {
-    rlang::abort(paste("A tabela", tabela, "não foi localizada"))
-    return(FALSE)
+
+    rlang::abort(glue::glue("A tabela {table} não foi localizada"))
+
   }
 
   # cria a conexão
+
   con <- DBI::dbConnect(
     drv = bigrquery::bigquery(),
     project = "basedosdados",
-    billing = billing_project_id
-  )
+    billing = billing_project_id)
 
   # chama o dplyr e retorna
+
   dplyr::tbl(con, tabela_completa)
 
 }
+
+# criar uma funçao interna que aplica collect()
+
+ # TODO documentar as duas juntamente na mesma página
+ # nos exemplos mostrar como elas se combinam
+ # o usuário pode gerar um lazy tbl, usar código típico de R para gerar uma query
+ # e usar bd_collect() para coletar os resultados
+
+bd_collect <- function(.lazy_tbl) {
+
+  # TODO verificar se o argumento `.lazy_tbl` é coletável
+  # retornar os resultados
+
+}
+
+
+
+bd_write <- function(.lazy_tbl, .write_fn = ? typed::Function(), ...) {
+
+  #' @param .write_fn é uma função de escrita
+  #'
+  #' tipagem com o typed se possível: https://github.com/moodymudskipper/typed
+
+
+  # TODO verificar se o argumento `.lazy_tbl` é coletável
+  # escrever os resultados
+
+  # esboço de como a escrita poderia ser parametrizada
+
+  # dúvidas com evaluation provavelmente serão sanadas aqui: https://adv-r.hadley.nz/evaluation.html#evaluation
+
+  data <- # resultado da coleção da query
+
+  rlang::call2(.write_fn, data, ...) %>%
+    rlang::eval_bare()
+
+}
+
+
