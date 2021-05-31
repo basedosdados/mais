092522a10096bc372dd5b612930b4a872f50d9d3
hellcassius <caiorogerio.santos@gmail.com>
2021-05-31 18:28:40 -0300

    fix attributes and define some helper functions to cut repetition

--- python-package/basedosdados/upload/validate.py
+++ python-package/basedosdados/upload/validate.py
@@ -7,7 +7,10 @@ from basedosdados.upload.table import Table
 from pathlib import Path
 import ckanapi
 import os
-import yaml
+from google.api_core.exceptions import NotFound
+import yaml, json
+import requests
+from yaml.loader import Loader
 
 CKAN_URL = os.environ.get("CKAN_URL", "http://localhost:5000")
 
@@ -36,33 +39,37 @@ class Metadata(Table):
 
         super().__init__(dataset_id, table_id, **kwargs)
 
-        self.table_metadata_path = Path(self.table_folder) / "table_config.yaml"
-        self.dataset_metadata_path = Path(self.dataset_folder) / "dataset_config.yaml"
-
-        with open(self.table_config, "r") as f:
-            table_config = yaml.load(f, Loader=NoDatesSafeLoader)
-
-        with open(self.dataset_config, "r") as d:
-            dataset_config = yaml.load(d, Loader=NoDatesSafeLoader)
-
-        self.table_config = table_config
+        self.table_metadata_path = Path(self.table_folder / "table_config.yaml")
+        self.dataset_metadata_path = self.dataset_folder / "dataset_config.yaml"
+        with open(self.dataset_metadata_path, "r", encoding="utf-8") as f:
+            dataset_config = yaml.load(f, Loader=yaml.SafeLoader)
         self.dataset_config = dataset_config
 
-    def _get_resource(self):
-        """Get resource by trying to match CKAN package name with given dataset_id and resource name with given table_id
+    def _get_package(self):
+        api_url = CKAN_URL + "/api/3/action/package_list"
+        result = requests.get(api_url)
+        pkg_list = json.loads(result.text)["result"]
 
-        Raises:
-            e: [description]
-
-        Returns:
-            dict: the resource dict as it is saved on the CKAN database.
-        """
+        if self.dataset_id in pkg_list:
+            dataset_id = self.dataset_id
+        elif self.dataset_id.replace("_", "-") in pkg_list:
+            dataset_id = self.dataset_id.replace("_", "-")
         try:
             pkg_dict = RemoteCKAN(
                 CKAN_URL, user_agent="", apikey=None
-            ).action.package_show(id=self.dataset_id)
+            ).action.package_show(id=dataset_id)
         except Exception as e:
             raise e
+        return pkg_dict
+
+    def _get_resource(self):
+        """Get resource by trying to match CKAN package name with given dataset_id and resource name with given table_id
+        Raises:
+            e: [description]
+        Returns:
+            dict: the resource dict as it is saved on the CKAN database.
+        """
+        pkg_dict = self._get_package()
         resource = [r for r in pkg_dict["resources"] if r["name"] == self.table_id][0]
         return resource
 
@@ -94,16 +101,12 @@ class Metadata(Table):
 
         if not path:
             dump_path = self.dataset_metadata_path
+        elif Path(path).is_dir():
+            dump_path = Path(path) / "dataset_config.yaml"
         else:
-            dump_path = Path(path)
-            if dump_path.is_file():
-                dump_path = dump_path.parent / "dataset_config.yaml"
-
-        pkg_dict = RemoteCKAN(CKAN_URL, user_agent="", apikey=None).action.package_show(
-            id=self.dataset_id
-        )
+            dump_path = Path(Path)
+        pkg_dict = self._get_package()
         pkg_dict.pop("resources")
-
         with open(dump_path, "w") as f:
             stream = yaml.dump(
                 pkg_dict,
@@ -123,28 +126,33 @@ class Metadata(Table):
                 as defined in your config.toml file.
                 Defaults to None.
         """
-        file = Path(path)
 
         resource = self._get_resource()
         if not path:
             dump_path = self.table_metadata_path
+        elif Path(path).is_dir():
+            dump_path = Path(path) / "table_config.yaml"
         else:
-            dump_path = Path(path)
-            if dump_path.is_file():
-                dump_path = dump_path.parent / "table_config.yaml"
+            dump_path = Path(Path)
         # Optionally select which keys will be dumped to generated metadata file
         # yaml_keys = [k for k in resource.keys()]
         with open(dump_path, "w") as f:
             stream = yaml.dump(
                 resource,
                 line_break=False,
-                sort_keys=False,
+                sort_keys=True,
                 default_flow_style=False,
                 allow_unicode=True,
             )
             f.write(stream)
 
     def validate_table(self):
+        """Validate table_config.yaml file. The yaml file should be located at metadata_path/dataset_id/table_id/, as defined in your config.toml
+
+        Raises:
+            BaseDosDadosException: when the file has validation errors.
+
+        """
         error_dict = {}
         self._check_resource_last_modified()
         try:
