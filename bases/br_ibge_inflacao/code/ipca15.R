1055ef7dfc46a00377ec57ce01e5ce9e55a7256e
pedrocastroo <pdesacastro@gmail.com>
2021-06-01 14:59:27 -0300

    br_ibge_inflacao

---
+++ bases/br_ibge_inflacao/code/ipca15.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(fs)
+
+fs::dir_create(c("input", "output"))
+
+utils::download.file(
+  url = "https://ftp.ibge.gov.br/Precos_Indices_de_Precos_ao_Consumidor/IPCA_15/Series_Historicas/ipca-15_SerieHist.zip",
+  destfile = "input/ipca15serie.zip"
+)
+
+utils::unzip("input/ipca15serie.zip", exdir = "input/")
+
+list.files("input/", pattern = ".xls", full.names = TRUE) %>% 
+  readxl::read_excel(., skip = 7) -> df
+
+months_br <- c(
+  "JAN" = 1, 
+  "FEV" = 2,
+  "MAR" = 3,
+  "ABR" = 4,
+  "MAI" = 5,
+  "JUN" = 6,
+  "JUL" = 7,
+  "AGO" = 8,
+  "SET" = 9,
+  "OUT" = 10,
+  "NOV" = 11,
+  "DEZ" = 12
+)
+
+df %>% 
+  dplyr::rename(
+    ano = ...1, 
+    mes = ...2, 
+    indice = ...3, 
+    var_mes = `NO MÊS`,
+    var_tres_meses = `3 MESES`,
+    var_semestral = SEMESTRAL,
+    var_anual = `NO ANO`,
+    var_doze_meses = `12 MESES`
+  ) %>% 
+  dplyr::filter(mes %in% names(months_br)) %>% 
+  tidyr::fill(ano) %>% 
+  dplyr::mutate(
+    mes = months_br[mes] %>% unname(),
+    dplyr::across(dplyr::everything(), as.numeric)
+  ) %>% 
+  readr::write_csv(file = "output/ipca15.csv")
