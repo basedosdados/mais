1cd7db02f224a551f242bc3e20c9ad1b1f65047c
Pedro Castro <pdesacastro@gmail.com>
2021-06-11 10:08:29 -0300

    [dados] br_rj_rio_de_janeiro_ipp_ips (#483)
    * add indicadores
    
    * add dimensoes_componentes
    
    * update br_rj_rio_de_janeiro_ipp_ips
    
    * fix br_rj_rio_de_janeiro_ipp_ips

---
+++ bases/br_rj_rio_de_janeiro_ipp_ips/dimensoes_componentes/publish.sql
@@ -0,0 +1,42 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+
+CREATE VIEW basedosdados-312117.br_rj_rio_de_janeiro_ipp_ips.dimensoes_componentes AS
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(regiao_administrativa AS STRING) regiao_administrativa,
+SAFE_CAST(ips_geral AS FLOAT64) ips_geral,
+SAFE_CAST(necessidades_humanas_basicas_nota_dimensao AS FLOAT64) necessidades_humanas_basicas_nota_dimensao,
+SAFE_CAST(nutricao_cuidados_medicos_basicos AS FLOAT64) nutricao_cuidados_medicos_basicos,
+SAFE_CAST(agua_saneamento AS FLOAT64) agua_saneamento,
+SAFE_CAST(moradia AS FLOAT64) moradia,
+SAFE_CAST(seguranca_pessoal AS FLOAT64) seguranca_pessoal,
+SAFE_CAST(fundamentos_bem_estar_nota_dimensao AS FLOAT64) fundamentos_bem_estar_nota_dimensao,
+SAFE_CAST(acesso_conhecimento_basico AS FLOAT64) acesso_conhecimento_basico,
+SAFE_CAST(acesso_informacao AS FLOAT64) acesso_informacao,
+SAFE_CAST(saude_bem_estar AS FLOAT64) saude_bem_estar,
+SAFE_CAST(qualidade_meio_ambiente AS FLOAT64) qualidade_meio_ambiente,
+SAFE_CAST(oportunidades_nota_dimensao AS FLOAT64) oportunidades_nota_dimensao,
+SAFE_CAST(direitos_individuais AS FLOAT64) direitos_individuais,
+SAFE_CAST(liberdades_individuais AS FLOAT64) liberdades_individuais,
+SAFE_CAST(tolerancia_inclusao AS FLOAT64) tolerancia_inclusao,
+SAFE_CAST(acesso_educacao_superior AS FLOAT64) acesso_educacao_superior
+from basedosdados-312117.br_rj_rio_de_janeiro_ipp_ips_staging.dimensoes_componentes as t
