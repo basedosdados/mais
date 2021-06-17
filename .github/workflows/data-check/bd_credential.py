1485fdfcc5259f5a6bf73b965e6d7e3bd2bf4d94
Vinicius Aguiar <vncsna@gmail.com>
2021-06-17 17:41:52 -0400

    [infra] add data checks (#445)
    * [infra] add draft of data checks
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * test action
    
    * trigger action
    
    * [infra] patch draft of data checks
    
    * [infra] fix previous patch
    
    * trigger data-check action
    
    * [infra] add pytest to Dockerfile
    
    * trigger data-check action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * trigger action
    
    * agora vai ... ou nao
    
    * agora vai ...
    
    * agora vai foi ...
    
    * trigger action
    
    * test two tables
    
    * test multiple tables
    
    * test multiple tables
    
    * test multiple tables
    
    * test multiple tables
    
    * reset configs modifications
    
    * [infra] add draft of automated pr comment
    
    * [infra] fix tab
    
    * [infra] fix output location
    
    * [infra] fix new typo
    
    * [infra] add pyarrow dependency
    
    * [infra] increase verbosity
    
    * test multiple tables
    
    * [infra] fix null test
    
    * revert "test multiple tables"
    
    this reverts commit 966c434263548521a4f7dff2ae5fc6177170e62e.
    
    * revert "trigger action"
    
    this reverts commit 575a1661e8e817b2ea251f94ad461c43d3acb23e.
    
    * [infra] add draft of data-check docs
    
    * change trigger
    
    * change trigger
    
    * change trigger
    
    * change trigger
    
    * change trigger
    
    * change trigger
    
    * change trigger
    
    * change trigger
    
    * change trigger
    
    * change trigger
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * force error
    
    * test multiple tables
    
    * [infra] new comment style
    
    * [infra] force errors
    
    * [infra] fix to comment header
    
    * [infra] revert forced triggers
    
    * [infra] fix bash expansion
    
    * [infra] refactoring
    
    * [infra] force trigger
    
    * [infra] fix new typo
    
    * [infra] add action shell
    
    * [infra] reverting to composite
    
    * [infra] fix brand new typo
    
    * [infra] modify report
    
    * [infra] reformat report and force errors
    
    * [infra] fix test
    
    * [infra] reformat report once again
    
    * [infra] add dataset and table names to query
    
    * change variable name
    
    * change markdown report
    
    * change description
    
    * fix typo
    
    * [infra] add data check docs
    
    * [infra] revert commit 
    
    Revert commit 50b99b5
    
    * revert "test multiple tables"
    
    revert commit 50b99b5
    
    * [infra] reverting new line at the end of the file
    
    Co-authored-by: d116626 <d116626@gmail.com>

---
+++ .github/workflows/data-check/bd_credential.py
@@ -0,0 +1,160 @@
+import base64
+import json
+import os
+import shutil
+import sys
+import traceback
+from pathlib import Path
+
+import basedosdados
+import basedosdados as bd
+import toml
+import tomlkit
+import yaml
+from basedosdados import Dataset, Storage
+from basedosdados.upload.base import Base
+from jinja2 import Template
+
+
+def decoding_base64(message):
+    # decoding the base64 string
+    base64_bytes = message.encode("ascii")
+    message_bytes = base64.b64decode(base64_bytes)
+    return message_bytes.decode("ascii")
+
+
+def create_config_folder(config_folder):
+    ## if ~/.basedosdados folder exists delete
+    if os.path.exists(Path.home() / config_folder):
+        shutil.rmtree(Path.home() / config_folder, ignore_errors=True)
+    ## create ~/.basedosdados folder
+    os.mkdir(Path.home() / config_folder)
+    os.mkdir(Path.home() / config_folder / "credentials")
+
+
+def save_json(json_obj, file_path, file_name):
+    ### function to save json file
+    with open(f"{file_path}/{file_name}", "w", encoding="utf-8") as f:
+        json.dump(json_obj, f, ensure_ascii=False, indent=2)
+
+
+def create_json_file(message_base64, file_name, config_folder):
+    ### decode base64 script and load as a json object
+    json_obj = json.loads(decoding_base64(message_base64))
+    prod_file_path = Path.home() / config_folder / "credentials"
+    ### save the json credential in the .basedosdados/credentials/
+    save_json(json_obj, prod_file_path, file_name)
+
+
+def save_toml(config_dict, file_name, config_folder):
+    ### save the config.toml in .basedosdados
+    file_path = Path.home() / config_folder
+    with open(file_path / file_name, "w") as toml_file:
+        toml.dump(config_dict, toml_file)
+
+
+def load_configs(dataset_id, table_id):
+    ### get the config file in .basedosdados/config.toml
+    configs_path = Base()._load_config()
+    ### get the path to metadata_path, where the folder bases with metadata information
+    metadata_path = configs_path["metadata_path"]
+    ### get the path to table_config.yaml
+    table_path = f"{metadata_path}/{dataset_id}/{table_id}"
+
+    return (
+        ### load the table_config.yaml
+        yaml.load(open(f"{table_path}/table_config.yaml", "r"), Loader=yaml.FullLoader),
+        ### return the path to .basedosdados configs
+        configs_path,
+    )
+
+
+def create_config_tree(prod_base64, staging_base64, config_dict):
+    ### execute the creation of .basedosdados
+    create_config_folder(".basedosdados")
+    ### create the prod.json secret
+    create_json_file(prod_base64, "prod.json", ".basedosdados")
+    ### create the staging.json secret
+    create_json_file(staging_base64, "staging.json", ".basedosdados")
+    ### create the config.toml
+    save_toml(config_dict, "config.toml", ".basedosdados")
+
+
+def get_table_dataset_id():
+    ### load the change files in PR || diff between PR and master
+    changes = json.load(Path("/home/runner/work/mais/mais/files.json").open("r"))
+    ### create a list to save the dataset and source_bucket related to each table_id
+    dataset_table_ids = []
+    ### create a list to save the table folder path, for each table changed in the commit
+    table_folders = []
+    for change_file in changes:
+        ### get the directory path for a table with changes
+        file_dir = Path(change_file).parent
+        ### append the table directory if it was not already appended
+        if file_dir not in table_folders:
+            table_folders.append(file_dir)
+    ### construct the iterable for the table_config paths
+    table_config_paths = [
+        Path(f"/home/runner/work/mais/mais/{root}/table_config.yaml")
+        for root in table_folders
+    ]
+    ### iterate through each config path
+    for filepath in table_config_paths:
+        ### check if the table_config.yaml exists in the changed folder
+        if filepath.is_file():
+            dataset_table_ids.append(filepath)
+        else:
+            print(
+                "\n###==============================================================================================###",
+                f"\n{str(filepath)} does not exist on current commit",
+                "\n###==============================================================================================###\n",
+            )
+    return dataset_table_ids
+
+
+def setup():
+    ### json with information of .basedosdados/config.toml
+    config_dict = {
+        "metadata_path": "/home/runner/work/mais/mais/bases",
+        "templates_path": str(Path.home() / ".basedosdados" / "templates"),
+        "bucket_name": "basedosdados-dev",
+        "gcloud-projects": {
+            "staging": {
+                "name": "basedosdados-dev",
+                "credentials_path": str(
+                    Path.home() / ".basedosdados" / "credentials" / "staging.json"
+                ),
+            },
+            "prod": {
+                "name": "basedosdados-dev",
+                "credentials_path": str(
+                    Path.home() / ".basedosdados" / "credentials" / "prod.json"
+                ),
+            },
+        },
+    }
+
+    ### load the secret of prod and staging data
+    prod_base64 = os.getenv("GCP_BD_DEV_PROD")
+    staging_base64 = os.getenv("GCP_BD_DEV_STAGING")
+
+    ### create config and credential folders
+    create_config_tree(prod_base64, staging_base64, config_dict)
+
+    ### create templates at config path
+    Base()._refresh_templates()
+
+    print(
+        "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ BASE $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+    )
+    print(
+        "\n",
+        Path(Path.home() / ".basedosdados" / "config.toml").open(mode="r").read(),
+        "\n",
+    )
+    ### Assert config files have been created
+    assert (Path.home() / ".basedosdados" / "config.toml").is_file()
+    assert (Path.home() / ".basedosdados" / "credentials" / "prod.json").is_file()
+    assert (Path.home() / ".basedosdados" / "credentials" / "staging.json").is_file()
+
+    return get_table_dataset_id()
