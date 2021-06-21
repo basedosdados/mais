3aaddf5d04c48bec30ad294f4228c7f1c5e20126
Lucas Moreira <lucas.nasc.m@gmail.com>
2021-06-21 17:48:01 -0300

    Sobe base de dados moveis

---
+++ bases/br_anatel_telefonia_movel/ddd/publish.sql
@@ -0,0 +1,31 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+
+CREATE VIEW input-dados.br_anatel_telefonia_movel.ddd AS
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(sigla_uf AS STRING) sigla_uf,
+SAFE_CAST(ddd AS INT64) ddd,
+SAFE_CAST(tecnologia AS STRING) tecnologia,
+SAFE_CAST(sinal AS STRING) sinal,
+SAFE_CAST(acessos AS INT64) acessos
+from input-dados.br_anatel_telefonia_movel_staging.ddd as t
