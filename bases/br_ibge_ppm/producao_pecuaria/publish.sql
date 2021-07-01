4b92f84c817ffe9eeb19735d122bfccf327bba2d
Hevilyn <hevilynsouza12@hotmail.com>
2021-07-01 16:27:58 -0300

    sobe revisao

--- bases/br_ibge_ppm/producao_pecuaria/publish.sql
+++ bases/br_ibge_ppm/producao_pecuaria/publish.sql
@@ -21,8 +21,8 @@ TIPOS:
 
 CREATE VIEW basedosdados-dev.br_ibge_ppm.producao_pecuaria AS
 SELECT 
-SAFE_CAST(id_municipio AS INT64) id_municipio,
 SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(vacas_ordenhadas AS INT64) vacas_ordenhadas,
 SAFE_CAST(ovinos_tosquiados AS INT64) ovinos_tosquiados
 from basedosdados-dev.br_ibge_ppm_staging.producao_pecuaria as t
