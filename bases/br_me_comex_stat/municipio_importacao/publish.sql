a9812a9f5008a168ef522e785baf6c13fd8ba415
Hevilyn Souza <77412631+hevsouza@users.noreply.github.com>
2021-07-06 18:04:57 -0300

    [dados-revisão] br_me_comex_stat (#549)

--- bases/br_me_comex_stat/municipio_importacao/publish.sql
+++ bases/br_me_comex_stat/municipio_importacao/publish.sql
@@ -22,10 +22,10 @@ CREATE VIEW basedosdados-dev.br_me_comex_stat.municipio_importacao AS
 SELECT 
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
-SAFE_CAST(id_sh4 AS INT64) id_sh4,
-SAFE_CAST(id_pais AS INT64) id_pais,
+SAFE_CAST(id_sh4 AS STRING) id_sh4,
+SAFE_CAST(id_pais AS STRING) id_pais,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
-SAFE_CAST(id_municipio AS INT64) id_municipio,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(peso_liquido_kg AS INT64) peso_liquido_kg,
 SAFE_CAST(valor_fob_dolar AS INT64) valor_fob_dolar
 from basedosdados-dev.br_me_comex_stat_staging.municipio_importacao as t
