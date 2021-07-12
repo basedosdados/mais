4752aa44b2f6ea5aa7fff862e64bfae95da944c3
Hevilyn <hevilynsouza12@hotmail.com>
2021-07-12 16:08:54 -0300

    revisao tabelas empresa e socio

--- bases/br_me_socios/empresa/publish.sql
+++ bases/br_me_socios/empresa/publish.sql
@@ -18,10 +18,10 @@ TIPOS:
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
 
 */
-CREATE VIEW `basedosdados.br_me_socios.empresa` AS
+CREATE VIEW `basedosdados-dev.br_me_socios.empresa` AS
 SELECT
 SAFE_CAST(CNPJ AS STRING) cnpj,
-SAFE_CAST(identificador_matriz_filial AS INT64) identificador_matriz_filial,
+SAFE_CAST(identificador_matriz_filial AS STRING) identificador_matriz_filial,
 SAFE_CAST(razao_social AS STRING) razao_social,
 SAFE_CAST(nome_fantasia AS STRING) nome_fantasia,
 SAFE_CAST(situacao_cadastral AS INT64) situacao_cadastral,
@@ -37,8 +37,8 @@ SAFE_CAST(numero AS STRING) numero,
 SAFE_CAST(complemento AS STRING) complemento,
 SAFE_CAST(bairro AS STRING) bairro,
 SAFE_CAST(CEP AS STRING) cep,
-SAFE_CAST(id_municipio_RF AS INT64) id_municipio_rf,
-SAFE_CAST(id_municipio AS INT64) id_municipio,
+SAFE_CAST(id_municipio_RF AS STRING) id_municipio_rf,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(estado_abrev AS STRING) sigla_uf,
 SAFE_CAST(DDD_telefone_1 AS STRING) ddd_telefone_1,
 SAFE_CAST(DDD_telefone_2 AS STRING) ddd_telefone_2,
@@ -52,4 +52,4 @@ SAFE_CAST(data_exclusao_do_simples AS DATE) data_exclusao_do_simples,
 SAFE_CAST(opcao_pelo_MEI AS INT64) opcao_pelo_mei,
 SAFE_CAST(situacao_especial AS STRING) situacao_especial,
 SAFE_CAST(data_situacao_especial AS DATE) data_situacao_especial
-from basedosdados-staging.br_me_socios_staging.empresa as t
+from basedosdados-dev.br_me_socios_staging.empresa as t
