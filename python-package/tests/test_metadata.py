3f7dc886869403c15a041840b0f628faee5bd9de
Vitor Mussa <vtrmussa@gmail.com>
2021-08-22 13:18:35 -0300

    Add more table metadata create tests

--- python-package/tests/test_metadata.py
+++ python-package/tests/test_metadata.py
@@ -58,3 +58,53 @@ def test_create_from_dataset_and_table_id(table_metadata, table_metadata_path):
     shutil.rmtree(table_metadata_path, ignore_errors=True)
     table_metadata.create()
     assert (table_metadata_path / METADATA_FILES['table']).exists()
+
+
+def test_create_if_exists_raise(dataset_metadata, table_metadata):
+    
+    with pytest.raises(FileExistsError):
+        dataset_metadata.create(if_exists="raise")
+
+    with pytest.raises(FileExistsError):
+        table_metadata.create(if_exists="raise")
+    
+
+def test_create_if_exists_replace(
+    dataset_metadata,
+    dataset_metadata_path,
+    table_metadata,
+    table_metadata_path
+    ):
+    dataset_metadata.create(if_exists="replace")
+    assert (dataset_metadata_path / METADATA_FILES['dataset']).exists()
+
+    table_metadata.create(if_exists="replace")
+    assert (table_metadata_path / METADATA_FILES['table']).exists()
+
+
+def test_create_if_exists_pass(
+    dataset_metadata,
+    dataset_metadata_path,
+    table_metadata,
+    table_metadata_path
+    ):
+
+    dataset_metadata.create(if_exists="replace")
+    assert (dataset_metadata_path / METADATA_FILES['dataset']).exists()
+
+    table_metadata.create(if_exists="replace")
+    assert (table_metadata_path / METADATA_FILES['table']).exists()    
+
+
+def test_create_columns(table_metadata, table_metadata_path):
+    shutil.rmtree(table_metadata_path, ignore_errors=True)
+    table_metadata.create(columns=["coluna1", "coluna2"])
+    assert(table_metadata_path / METADATA_FILES['table']).exists()
+    
+
+def test_create_partition_columns():
+    pass
+
+
+def test_create_force_columns():
+    pass
