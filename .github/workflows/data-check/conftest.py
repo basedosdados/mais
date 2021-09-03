86282422db37fdd337ad66570c567a462271ba7b
Vinicius <vncsna@gmail.com>
2021-09-02 20:46:53 -0400

    [infra] altera o nome para project_id_prod

--- .github/workflows/data-check/conftest.py
+++ .github/workflows/data-check/conftest.py
@@ -105,7 +105,7 @@ def pytest_sessionstart(session):
     with open(check_path, "r", encoding="utf-8") as file:
         skeleton = file.read()
         if os.environ.get("IS_PROD_ENV", False):
-            skeleton = skeleton.replace("{{ project_id_staging }}", "basedosdados")
+            skeleton = skeleton.replace("{{ project_id_staging }}", "{{ project_id_prod }}")
         checks = Template(skeleton)
 
     # load checks with configs from table_config.yaml
