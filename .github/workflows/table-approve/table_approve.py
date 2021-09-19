2e6c9c5c7f62adb665c3b5ff282a369d028d3932
d116626 <d116626@gmail.com>
2021-09-18 22:46:11 -0300

    fix(infra): replace if header exists in storage

--- .github/workflows/table-approve/table_approve.py
+++ .github/workflows/table-approve/table_approve.py
@@ -218,7 +218,7 @@ def push_table_to_bq(
     df = bd.read_sql(query, billing_project_id="basedosdados-dev", from_file=True)
     df.to_csv("header.csv", index=False, encoding="utf=8")
     st = bd.Storage(dataset_id=dataset_id, table_id=dataset_id)
-    st.upload("header.csv", mode="header")
+    st.upload("header.csv", mode="header", if_exists="replace")
 
 
 def pretty_log(dataset_id, table_id, source_bucket_name):
