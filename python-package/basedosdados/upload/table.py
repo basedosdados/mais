8b1d6f94c5b9495800de7f6c69a3a0b6ccbeca75
d116626 <d116626@gmail.com>
2021-09-21 08:44:32 -0300

    fix(infra): clean spaces and put comma

--- python-package/basedosdados/upload/table.py
+++ python-package/basedosdados/upload/table.py
@@ -5,6 +5,8 @@ import csv
 from copy import deepcopy
 from google.cloud import bigquery
 import datetime
+import textwrap
+import inspect
 
 import ruamel.yaml as ryaml
 import requests
@@ -157,12 +159,15 @@ class Table(Base):
             - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
             - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
         */
-
         """
+
+        # remove triple quotes extra space
+        publish_txt = inspect.cleandoc(publish_txt)
+        publish_txt = textwrap.dedent(publish_txt)
 
         # add create table statement
         project_id_prod = self.client["bigquery_prod"].project
-        publish_txt += f"CREATE VIEW {project_id_prod}.{self.dataset_id}.{self.table_id} AS\nSELECT \n"
+        publish_txt += f"\n\nCREATE VIEW {project_id_prod}.{self.dataset_id}.{self.table_id} AS\nSELECT \n"
 
         # sort columns by is_partition, partitions_columns come first
         columns = sorted(
@@ -176,7 +181,9 @@ class Table(Base):
                 "STRING" if col["bigquery_type"] is None else col["bigquery_type"]
             )
 
-            publish_txt += f"SAFE_CAST({name} AS {bigquery_type}) {name}\n"
+            publish_txt += f"SAFE_CAST({name} AS {bigquery_type}) {name},\n"
+        ## remove last comma
+        publish_txt = publish_txt[:-2] + "\n"
 
         # add from statement
         project_id_staging = self.client["bigquery_staging"].project
