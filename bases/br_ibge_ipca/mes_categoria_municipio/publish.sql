31baa1f60c22a41fea2577e1d45adaaf6263136e
gustavoairestiago <gustavo.tiago@usp.br>
2021-10-10 16:37:19 -0300

    Sobe Dataset e Tabelas IPCA
    Contém tabela IPCA por mes (desde 1979) e IPCA por produto (2020-2021) em RM, município e Brasil

---
+++ bases/br_ibge_ipca/mes_categoria_municipio/publish.sql
@@ -0,0 +1,34 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+
+CREATE VIEW basedosdados-dev.br_ibge_ipca.mes_categoria_municipio AS
+SELECT 
+SAFE_CAST(ano AS STRING) ano,
+SAFE_CAST(mes AS STRING) mes,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
+SAFE_CAST(id_categoria AS STRING) id_categoria,
+SAFE_CAST(id_categoria_bd AS STRING) id_categoria_bd,
+SAFE_CAST(categoria AS STRING) categoria,
+SAFE_CAST(peso_mensal AS STRING) peso_mensal,
+SAFE_CAST(variacao_mensal AS STRING) variacao_mensal,
+SAFE_CAST(variacao_anual AS STRING) variacao_anual,
+SAFE_CAST(variacao_doze_meses AS STRING) variacao_doze_meses
+from basedosdados-dev.br_ibge_ipca_staging.mes_categoria_municipio as t
