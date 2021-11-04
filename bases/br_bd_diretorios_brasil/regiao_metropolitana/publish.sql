152b0f1121a647cf09a03c42504c47d1781da603
gustavoairestiago <gustavo.tiago@usp.br>
2021-11-03 23:00:03 -0300

    br_bd_diretorios_brasil.regiao_metropolitana

---
+++ bases/br_bd_diretorios_brasil/regiao_metropolitana/publish.sql
@@ -0,0 +1,29 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+
+CREATE VIEW basedosdados-dev.br_bd_diretorios_brasil.regiao_metropolitana AS
+SELECT 
+SAFE_CAST(nome_regiao AS STRING) nome_regiao,
+SAFE_CAST(sigla_uf AS STRING) sigla_uf,
+SAFE_CAST(id_regiao_metropolitana AS STRING) id_regiao_metropolitana,
+SAFE_CAST(nome AS STRING) nome,
+SAFE_CAST(tipo AS STRING) tipo
+from basedosdados-dev.br_bd_diretorios_brasil_staging.regiao_metropolitana as t
