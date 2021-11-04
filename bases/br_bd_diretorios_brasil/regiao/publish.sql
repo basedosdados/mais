3ef019a107fbbb748ce064144508d841f6d3847b
Gustavo Aires Tiago <36206956+gustavoairestiago@users.noreply.github.com>
2021-11-03 22:25:00 -0300

    [dados] br_bd_diretorios_brasil.regiao (#920)
    * Sobe Região Brasil
    
    Tabela de grandes regiões brasileiras
    
    * Sobe tabela regiao em diretorios_brasil
    
    Sobe com alteração de nome
    
    * Sobe publish
    
    * Altera sigla_regiao para sigla

---
+++ bases/br_bd_diretorios_brasil/regiao/publish.sql
@@ -0,0 +1,26 @@
+/*
+
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
+*/
+
+CREATE VIEW basedosdados-dev.br_bd_diretorios_brasil.regiao AS
+SELECT 
+SAFE_CAST(sigla AS STRING) sigla,
+SAFE_CAST(nome AS STRING) nome
+from basedosdados-dev.br_bd_diretorios_brasil_staging.regiao as t
