06e6c563a55abc5eb01a94b48612884f61039d13
Crislane A. Souza <s.alvescrislane@gmail.com>
2021-12-09 20:58:09 -0300

    [dados] pnad_populacao

---
+++ bases/br_ibge_pnadc/ano_regiao_metropolitana_raca_cor/publish.sql
@@ -0,0 +1,27 @@
+/*
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+*/
+
+CREATE VIEW basedosdados-dev.br_ibge_pnadc.ano_regiao_metropolitana_raca_cor AS
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(id_regiao_metropolitana AS STRING) id_regiao_metropolitana,
+SAFE_CAST(sexo AS STRING) sexo,
+SAFE_CAST(cor_raca AS STRING) cor_raca,
+SAFE_CAST(populacao AS INT64) populacao
+FROM basedosdados-dev.br_ibge_pnadc_staging.ano_regiao_metropolitana_raca_cor AS t
