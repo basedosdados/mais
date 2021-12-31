a7c9d22b94b86421ec70022d4b3303c2403364a3
Lucas Moreira <lucas.nasc.m@gmail.com>
2021-12-30 22:00:46 -0300

    atualiza dados de banda larga fixa

--- bases/br_anatel_banda_larga_fixa/microdados/publish.sql
+++ bases/br_anatel_banda_larga_fixa/microdados/publish.sql
@@ -1,5 +1,4 @@
 /*
-
 Query para publicar a tabela.
 
 Esse é o lugar para:
@@ -16,7 +15,6 @@ TIPOS:
     - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
     - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
-
 */
 
 CREATE VIEW basedosdados-dev.br_anatel_banda_larga_fixa.microdados AS
