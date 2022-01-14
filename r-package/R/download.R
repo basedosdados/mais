f86ae58be5860528e07a341909ebcad92456ac6c
Matheus Valentim <75315649+mavalentim@users.noreply.github.com>
2022-01-14 19:11:03 +0100

    redoing dict function as info function

--- r-package/R/download.R
+++ r-package/R/download.R
@@ -227,3 +227,106 @@ read_sql <- function(
 
 
 
+#' Access a data sets' most relevant information
+#'
+#' @param dataset String, name of a data set.
+#' @param option Binary. "Dictionary" will output the dataset's dictionary in tibble format. "Browse" will launch the data set's webpage.
+#' @param billing_project_id a string containing your billing project id. If you've run `set_billing_id` then feel free to leave this empty.
+#'
+#' @return Returns information on a data set. If you want to check for factors' and variables' meanings, set `option` to "Dictionary".
+#' If you prefer to check the data set's full webpage, where you can access the data sets' geographic and chronological range,
+#' available data tables and Base dos Dados' members who are in charge of this data set, set `option` to "Browse".
+#'
+#'
+#'
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' #Checking factors
+#' dictionary("br_inep_censo_escolar")
+#'
+#' dictionary("br_me_rais")
+#'
+#' #Checking for was responsible for uploading this dataset
+#'
+#'
+#' }
+#'
+#'
+#' @import bigrquery
+#' @import glue
+#' @importFrom rlang abort is_character
+#' @importFrom magrittr %>%
+#' @export
+#'
+#'
+
+# dictionary <- function(
+#   dataset,
+#   billing_project_id = get_billing_id(),
+#   page_size = 100000) {
+#
+#   if(billing_project_id == FALSE) {
+#
+#     rlang::abort("You haven't set a Project Billing Id. Use the function `set_billing_id` to do so.")
+#
+#   }
+#
+#   if(!rlang::is_string(query)) {
+#
+#     rlang::abort("`query` argument must be a string.")
+#
+#   }
+#
+#   bigrquery::bq_project_query(
+#     billing_project_id,
+#     query = glue::glue("SELECT *
+#                   FROM basedosdados.{dataset}.dicionario")) %>%
+#     bigrquery::bq_table_download(bigint = "integer64")
+#
+# }
+
+
+
+dataset_help <- function(dataset,
+                         option = "dictionary",
+                         billing_project_id = get_billing_id()){
+
+  #first, throw an error if dataset is not a string
+  if(!rlang::is_string(dataset)){
+    rlang::abort(" 'dataset' must be a string")
+  }
+
+  #second, throw an error if the billing_project_id is not correct
+  if(billing_project_id == FALSE) {
+
+    rlang::abort("You haven't set a Project Billing Id. Use the function `set_billing_id` to do so.")
+
+  }
+
+  #Now the actual function
+
+  #Option 1 : open the datasets' dictionary
+  if(option == "dictionary"){
+
+    #else, open the dict baby
+    bigrquery::bq_project_query(
+      billing_project_id,
+      query = glue::glue("SELECT *
+                  FROM basedosdados.{dataset}.dicionario")) %>%
+      bigrquery::bq_table_download(bigint = "integer64")
+
+
+  }
+
+  #Option 2: browse the web for more info on the dataset
+  else if(option == 'browse'){
+    base_url <- "https://basedosdados.org/dataset/"
+    endpoint <- stringr::str_replace_all(dataset,"_","-")
+    browseURL(paste0(base_url,endpoint))
+  }
+}
+
+
