539565abc5176785f6f0c9b6da0a2fe53f941d7b
crislanealves <crislanealves@protonmail.com>
2022-02-16 15:23:04 -0300

    [dados] br_sp_dieese_icv

---
+++ bases/br_sp_dieese_icv/mes_sp/publish.sql
@@ -0,0 +1,26 @@
+/*
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+*/
+
+CREATE VIEW basedosdados-dev.br_sp_dieese_icv.mes_sp AS
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(indice AS FLOAT64) indice,
+SAFE_CAST(variacao_mensal AS FLOAT64) variacao_mensal
+FROM basedosdados-dev.br_sp_dieese_icv_staging.mes_sp AS t
