750e92cb5886d2793d556012a434ede329128f1b
crislanealves <crislanealves@protonmail.com>
2022-02-21 17:13:34 -0300

    sobe icv

---
+++ bases/br_sp_saopaulo_dieese_icv/ano/publish.sql
@@ -0,0 +1,24 @@
+/*
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+*/
+
+CREATE VIEW basedosdados-dev.br_sp_saopaulo_dieese_icv.ano AS
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(variacao_anual AS FLOAT64) variacao_anual
+FROM basedosdados-dev.br_sp_saopaulo_dieese_icv_staging.ano AS t
