9f670dccd7e607b12249f82bd4855d721a22c7dc
Lucas Moreira <lucas.nasc.m@gmail.com>
2022-03-28 21:33:09 -0300

    corrige metadata modified

--- bases/br_anatel_banda_larga_fixa/backhaul/publish.sql
+++ bases/br_anatel_banda_larga_fixa/backhaul/publish.sql
@@ -1,4 +1,5 @@
 /*
+
 Query para publicar a tabela.
 
 Esse é o lugar para:
@@ -15,17 +16,18 @@ TIPOS:
     - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
     - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+
 */
 
-CREATE VIEW basedosdados-dev.br_anatel_banda_larga_fixa.backhaul AS
+CREATE VIEW input-dados.br_anatel_banda_larga_fixa.backhaul AS
 SELECT 
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(situacao AS STRING) situacao,
-SAFE_CAST(ano_atendimento AS INT64) ano_atendimento,
+SAFE_CAST(ano_atendimento AS STRING) ano_atendimento,
 SAFE_CAST(concessionaria AS STRING) concessionaria,
 SAFE_CAST(tecnologia AS STRING) tecnologia,
-SAFE_CAST(capacidade_backhaul AS INT64) capacidade_backhaul,
-SAFE_CAST(capacidade_ocupada AS FLOAT64) capacidade_ocupada,
-SAFE_CAST(capacidade_disponivel AS FLOAT64) capacidade_disponivel
-FROM basedosdados-dev.br_anatel_banda_larga_fixa_staging.backhaul AS t
+SAFE_CAST(capacidade_backhaul AS STRING) capacidade_backhaul,
+SAFE_CAST(capacidade_ocupada AS STRING) capacidade_ocupada,
+SAFE_CAST(capacidade_disponivel AS STRING) capacidade_disponivel
+from input-dados.br_anatel_banda_larga_fixa_staging.backhaul as t
