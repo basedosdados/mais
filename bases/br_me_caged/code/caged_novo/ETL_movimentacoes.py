9fba9df1e15038f69feeb5347ca61319941eac2b
Isadora Bugarin <isadorabugarin@gmail.com>
2022-04-18 19:53:12 -0300

    checkpoint

--- bases/br_me_caged/code/caged_novo/ETL_movimentacoes.py
+++ bases/br_me_caged/code/caged_novo/ETL_movimentacoes.py
@@ -25,18 +25,20 @@ today = datetime.strftime(datetime.today(), "%Y-%m-%d")
 RAW_PATH = "../data/caged_novo/raw/"
 CLEAN_PATH = "../data/caged_novo/clean/"
 
-# GET MUNICIPIOS FROM BD
-print("====== ", today, " ======")
-query = """
-SELECT 
-    sigla_uf,
-    id_municipio,
-    id_municipio_6
-FROM `basedosdados.br_bd_diretorios_brasil.municipio` 
-"""
+def get_municipios():
+    # GET MUNICIPIOS FROM BD
+    print("====== ", today, " ======")
+    query = """
+    SELECT 
+        sigla_uf,
+        id_municipio,
+        id_municipio_6
+    FROM `basedosdados.br_bd_diretorios_brasil.municipio` 
+    """
 
-municipios = bd.read_sql(query, billing_project_id="basedosdados-dev")
-print("\n")
+    municipios = bd.read_sql(query, billing_project_id="basedosdados-dev")
+    print("\n")
+    return municipios
 
 # deleta pasta
 if os.path.isdir(CLEAN_PATH):
@@ -46,61 +48,42 @@ if os.path.isdir(RAW_PATH):
 
 print("Get download links")
 download_dict = caged_novo.get_download_links()
-print("\n")
+tb = bd.Table(dataset_id="br_me_caged", table_id=f"microdados_movimentacoes")
+st = bd.Storage(table_id="microdados_movimentacoes", dataset_id="br_me_caged")
 
-for tipo in list(download_dict.keys()):
+def teste():
+    if download_dict != {}:
+        for year_month_path in list(download_dict.keys()):
 
-    download_opt = caged_novo.get_filtered_download_dict(
-        tipo=tipo, download_dict=download_dict, bucket_name="basedosdados-dev"
-    )
-
-    print(f"Filter download links {tipo}")
-
-    trigger, download_opt = caged_novo.get_trigger_and_download_opt(download_opt, tipo)
-
-    if trigger:
-        for year_month_path in list(download_opt.keys()):
-            print(tipo, ": ", year_month_path)
-            ## download data
-            save_path = RAW_PATH + f"{tipo}/" + year_month_path
-            caged_novo.download_data(save_path, download_opt[year_month_path])
+            save_path = RAW_PATH + "movimentacoes/" + str(year_month_path)
+            caged_novo.download_data(save_path, download_dict[year_month_path])
             
-            #create some vars
-            file_name = download_opt[year_month_path].split("/")[-1].split(".")[0]
-            file_path = RAW_PATH + f"{tipo}/" + year_month_path
-            
-#             caged_novo.upload_to_raw(tipo=tipo, save_raw_path=f"{save_path}{file_name}.7z")
-
-
-            # extrai arquivo
+            file_name = download_dict[year_month_path].split("/")[-1].split(".")[0]
+            file_path = RAW_PATH + "movimentacoes" + str(year_month_path)
+             
             caged_novo.extract_file(file_path, file_name, save_rows=None)
 
-            # load e organiza os dados
-            df = caged_novo.rename_add_orginaze_columns(file_path, file_name, tipo, municipios)
+            municipios = get_municipios
+            df = caged_novo.rename_add_orginaze_columns(file_path, file_name, municipios)
 
-            # salva no formato de particao
-            save_clean_path = CLEAN_PATH + f"{tipo}/"
+            save_clean_path = CLEAN_PATH + "movimentacoes/"
             caged_novo.creat_partition(df, save_clean_path, year_month_path)
 
-            # upload basedosdados
-#             caged_novo.upload_to_bd(tipo, save_clean_path)
 
-            # deleta pasta
-#             shutil.rmtree(CLEAN_PATH + f"{tipo}/")
-#             shutil.rmtree(RAW_PATH + f"{tipo}/")
-
-    print("\n")
-
+teste()
 #CREATE TABLE AND FILL METADATA
-tb = bd.Table(table_id = 'microdados_movimentacoes',dataset_id = 'br_me_caged')
-st = bd.Storage('br_me_caged','microdados_movimentacoes',)
-st.delete_table(bucket_name='basedosdados-dev')
+print('IM HERE')
+tb = bd.Table(table_id = 'microdados_movimentacoes', dataset_id = 'br_me_caged')
+print('IM HERE')
+st = bd.Storage('br_me_caged','microdados_movimentacoes')
+print('IM HERE')
+#st.delete_table(bucket_name='basedosdados-dev')
 tb.create(path='../data/caged_novo/clean/movimentacoes/',
           if_table_config_exists='pass',
           if_storage_data_exists='replace',
           if_table_exists='replace'
          )
-
+print('IM HERE')
 tb.update_columns('https://docs.google.com/spreadsheets/d/1ihyOCSkaarmR3uMHj8bmP9tv-OChVeL2LkEWaGqxp7w/edit#gid=787251136')
 
 tb.publish(if_exists='replace')
