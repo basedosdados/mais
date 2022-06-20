dc7e0e6566429c8628924353c83dee939eacaa27
lucascr91 <lucas.ecomg@gmail.com>
2022-06-20 18:47:27 -0300

    [infra] add tests for Datatype class

--- python-package/basedosdados/upload/datatypes.py
+++ python-package/basedosdados/upload/datatypes.py
@@ -22,22 +22,20 @@ class Datatype:
 
         if self.source_format == "csv":
             return next(csv.reader(open(data_sample_path, "r", encoding="utf-8")))
-
-        elif self.source_format == "avro":
+        if self.source_format == "avro":
             dataframe = pandavro.read_avro(str(data_sample_path))
             return list(dataframe.columns.values)
-
-        elif self.source_format == "parquet":
+        if self.source_format == "parquet":
             dataframe = pd.read_parquet(str(data_sample_path))
             return list(dataframe.columns.values)
-
-        else:
-            raise NotImplementedError(
-                "Base dos Dados just supports comma separated csv, avro and parquet files"
-            )
+        raise NotImplementedError(
+            "Base dos Dados just supports comma separated csv, avro and parquet files"
+        )
 
     def partition(self):
-
+        '''
+        Configure the partitioning of the table
+        '''
         hive_partitioning = bigquery.external_config.HivePartitioningOptions()
         hive_partitioning.mode = "AUTO"
         hive_partitioning.source_uri_prefix = self.table_obj.uri.format(
@@ -48,34 +46,27 @@ class Datatype:
 
     @property
     def external_config(self):
-
+        '''
+        Configure the external table
+        '''
         if self.source_format == "csv":
-
             _external_config = bigquery.ExternalConfig("CSV")
             _external_config.options.skip_leading_rows = 1
             _external_config.options.allow_quoted_newlines = True
             _external_config.options.allow_jagged_rows = True
             _external_config.autodetect = False
             _external_config.schema = self.table_obj._load_schema(self.mode)
-
         elif self.source_format == "avro":
-
             _external_config = bigquery.ExternalConfig("AVRO")
-
         elif self.source_format == "parquet":
-
             _external_config = bigquery.ExternalConfig("PARQUET")
-
         else:
-
             raise NotImplementedError(
                 "Base dos Dados just supports comma separated csv, avro and parquet files"
             )
 
         _external_config.source_uris = f"gs://{self.table_obj.bucket_name}/staging/{self.table_obj.dataset_id}/{self.table_obj.table_id}/*"
-
         if self.partitioned:
-
             _external_config.hive_partitioning = self.partition()
 
         return _external_config
