887c2275e400b1293d4608340d73769a73047344
lucascr91 <lucas.ecomg@gmail.com>
2022-07-01 19:08:48 -0300

    [ínfra] refactor subprocess run

--- python-package/tests/test_cli.py
+++ python-package/tests/test_cli.py
@@ -28,6 +28,30 @@ TABLE_FILES = ["publish.sql", "table_config.yaml"]
 
 cli_dir = Path(__file__).parent / ".." / "basedosdados" / "cli"
 
+def _run_command(command, output="err"):
+    """
+    Run a command and return the output
+    """
+
+    current_dir = os.getcwd()
+
+    os.chdir(cli_dir)
+    proc = subprocess.Popen(
+        [command],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+
+    out, err = proc.communicate()
+
+    os.chdir(current_dir)
+
+    if output == "err":
+        return err
+    if output == "out":
+        return out
+
 
 def table_exists(table, mode):
     """
@@ -45,96 +69,36 @@ def test_cli_dataset_create():
     Test create dataset command
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli dataset create {DATASET_ID} --if_exists=replace"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    out, _ = proc.communicate()
-
-    os.chdir(current_dir)
+    out = _run_command(f"python3 -m cli dataset create {DATASET_ID} --if_exists=replace", output="out")
 
     assert b"Datasets `pytest` and `pytest_staging` were created in BigQuery" in out
+
+def test_cli_dataset_publicize():
+    """
+    Test publicize dataset command
+    """
+
+    out = _run_command(f"python3 -m cli dataset publicize {DATASET_ID}", output="out")
+
+    assert b"Dataset `pytest` became public!" in out
 
 def test_cli_dataset_delete():
     """
     Test delete dataset command
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f" yes | python3 -m cli dataset delete {DATASET_ID}"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    out, _ = proc.communicate()
-
-    os.chdir(current_dir)
+    out = _run_command(f"yes | python3 -m cli dataset delete {DATASET_ID}", output="out")
 
     assert b"Datasets `pytest` and `pytest_staging` were deleted in BigQuery" in out
 
-def test_cli_dataset_init(testdir):
+def test_cli_dataset_init():
     """
     Test init dataset command
     """
 
-    current_dir = os.getcwd()
-
-    shutil.rmtree(testdir / DATASET_ID / TABLE_ID, ignore_errors=True)
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli dataset init {DATASET_ID} --replace"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    out, _ = proc.communicate()
-
-    os.chdir(current_dir)
+    out = _run_command(f"python3 -m cli dataset init {DATASET_ID} --replace", output="out")
 
     assert b"Dataset `pytest` folder and metadata were created at" in out
-
-def test_cli_dataset_publicize(testdir):
-    """
-    Test publicize dataset command
-    """
-
-    current_dir = os.getcwd()
-
-    shutil.rmtree(testdir / DATASET_ID / TABLE_ID, ignore_errors=True)
-    os.chdir(cli_dir)
-    os.system(f"python3 -m cli dataset create {DATASET_ID} --replace")
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli dataset publicize {DATASET_ID}"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    out, _ = proc.communicate()
-
-    os.chdir(current_dir)
-
-    assert b"Dataset `pytest` became public!" in out
-
 
 @pytest.mark.skip(reason="require positional argument not explicited by help")
 def test_cli_dataset_update():
@@ -142,21 +106,7 @@ def test_cli_dataset_update():
     Test update dataset command
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli dataset update {DATASET_ID}"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    out, _ = proc.communicate()
-
-    os.chdir(current_dir)
+    out = _run_command(f"python3 -m cli dataset update {DATASET_ID}", output="out")
 
     assert b"Dataset `pytest` was updated in BigQuery" in out
 
@@ -165,21 +115,7 @@ def test_cli_download(testdir):
     Test download command
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f'''python3 -m cli download {testdir /  "data.csv"}  --query="select * from basedosdados.br_ibge_pib.municipio limit 10" --billing_project_id=basedosdados-dev'''
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    _, err = proc.communicate()
-
-    os.chdir(current_dir)
+    _ = _run_command(f'''python3 -m cli download {testdir /  "data.csv"}  --query="select * from basedosdados.br_ibge_pib.municipio limit 10" --billing_project_id=basedosdados-dev''', output="out")
 
     assert os.path.isfile(testdir / "data.csv")
 
@@ -190,21 +126,7 @@ def test_cli_get_data_description(testdir):
     Teste if error is raised when no path is provided
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli get dataset_description br_me_rais"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    _, err = proc.communicate()
-
-    os.chdir(current_dir)
+    err = _run_command(f"python3 -m cli get_data_description br_me_rais", output="err")
 
     assert b"Description" in err
 
@@ -214,21 +136,7 @@ def test_cli_get_table_columns(testdir):
     Teste if error is raised when no path is provided
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli get table_columns br_me_rais microdados"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    _, err = proc.communicate()
-
-    os.chdir(current_dir)
+    err = _run_command(f"python3 -m cli get_table_columns br_me_rais microdados", output="err")
 
     assert b"Columns" in err
 
@@ -239,21 +147,7 @@ def test_cli_get_table_description(testdir):
     Teste if error is raised when no path is provided
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli get table_description br_me_rais microdados"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    _, err = proc.communicate()
-
-    os.chdir(current_dir)
+    err = _run_command(f"python3 -m cli get_table_description br_me_rais microdados", output="err")
 
     assert b"Description" in err
 
@@ -263,21 +157,7 @@ def test_cli_list_dataset_tables():
     Teste if error is raised when no path is provided
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli list dataset_tables br_me_rais"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    _, err = proc.communicate()
-
-    os.chdir(current_dir)
+    err = _run_command(f"python3 -m cli list_dataset_tables br_me_rais", output="err")
 
     assert b"Tables" in err
 
@@ -287,23 +167,19 @@ def test_cli_list_datasets():
     Teste if error is raised when no path is provided
     """
 
-    current_dir = os.getcwd()
-
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli list datasets"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    _, err = proc.communicate()
-
-    os.chdir(current_dir)
+    err = _run_command(f"python3 -m cli list_datasets", output="err")
 
     assert b"Datasets" in err
+
+def test_cli_metadata_create():
+    """
+    Teste if error is raised when no path is provided
+    """
+
+    _ = _run_command(f"python3 -m cli metadata create --if_exists=replace {DATASET_ID} {TABLE_ID}", output="out")
+
+    assert os.path.isfile(testdir / "data.csv")
+
 
 
 def test_cli_create_no_path_error(testdir):
@@ -311,22 +187,7 @@ def test_cli_create_no_path_error(testdir):
     Teste if error is raised when no path is provided
     """
 
-    current_dir = os.getcwd()
-
-    shutil.rmtree(testdir / DATASET_ID / TABLE_ID, ignore_errors=True)
-    os.chdir(cli_dir)
-    proc = subprocess.Popen(
-        [
-            f"python3 -m cli --metadata_path={testdir} table create --if_table_exists=pass {DATASET_ID} {TABLE_ID}"
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True,
-    )
-
-    _, err = proc.communicate()
-
-    os.chdir(current_dir)
+    err = _run_command(f"python3 -m cli --metadata_path={testdir} table create --if_table_exists=pass {DATASET_ID} {TABLE_ID}", output="err")
 
     assert b"You must provide a path to correctly create config files" in err
 
