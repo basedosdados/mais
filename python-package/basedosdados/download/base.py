dbb045419a3a1d3e968c48608ca9b417241a3a39
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-08-12 19:07:42 -0300

    automatically authenticate from colab or local

--- python-package/basedosdados/download/base.py
+++ python-package/basedosdados/download/base.py
@@ -4,7 +4,7 @@ Functions for manage auth and credentials
 # pylint: disable=redefined-outer-name, protected-access
 from functools import lru_cache
 
-import google.auth
+import sys
 
 from google.cloud import bigquery, storage
 import pydata_google_auth
@@ -29,24 +29,30 @@ def reauth():
 
 def credentials(from_file=False, reauth=False):
     '''
-    Get user credentials with google.auth
+    Get user credentials
     '''
 
+    # check if is running in colab
+    if "google.colab" in sys.modules:
+        # authenticate from google colab
+
+        # pylint: disable=no-name-in-module, import-outside-toplevel, import-error
+        from google.colab import auth
+        auth.authenticate_user()
+        return None
+
+    #Authenticate from PyData, for compatibility reasons
     if from_file:
         return Base()._load_credentials(mode="prod")
 
     if reauth:
-        credentials_, _ = google.auth.default(
-            scopes=SCOPES,
-            # credentials_cache=None,
+        return pydata_google_auth.get_user_credentials(
+            SCOPES, credentials_cache=pydata_google_auth.cache.REAUTH
         )
-        return credentials_
 
-    credentials_, _ = google.auth.default(
-        scopes=SCOPES
+    return pydata_google_auth.get_user_credentials(
+            SCOPES,
     )
-    return credentials_
-
 
 
 @lru_cache(256)
