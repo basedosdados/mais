ce7c6986ee6a1b0765a8c27f336972e37438ca7f
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-09-20 16:28:46 -0300

    [dados] br_fgv_igp (#1322)
    * metadata of FGV IGP tables
    
    * improving fields descriptions
    
    * indentation
    
    * code review - first commit
    
    * removing month from observation level in igp_m_ano
    
    * removing month from observation level in igp_og_ano
    
    * correcting table title in IGP-10 Mês
    
    * changing field names
    
    * adding dataset_config.yaml and data_cleaning_code_url in table_config.yaml for tables
    
    * corrections in table_config.yaml
    
    * sql files with correct fields formats
    
    * updating json schema and txt description files
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- bases/br_fgv_igp/igp_m_mes/publish.sql
+++ bases/br_fgv_igp/igp_m_mes/publish.sql
@@ -1,5 +1,4 @@
 /*
-
 Query para publicar a tabela.
 
 Esse é o lugar para:
@@ -16,7 +15,6 @@ TIPOS:
     - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
     - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
-
 */
 
 CREATE VIEW basedosdados-dev.br_fgv_igp.igp_m_mes AS
@@ -25,6 +23,9 @@ SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
 SAFE_CAST(indice AS FLOAT64) indice,
 SAFE_CAST(variacao_mensal AS FLOAT64) variacao_mensal,
+SAFE_CAST(variacao_12_meses AS FLOAT64) variacao_12_meses,
 SAFE_CAST(variacao_primeiro_decendio AS FLOAT64) variacao_primeiro_decendio,
-SAFE_CAST(variacao_segundo_decendio AS FLOAT64) variacao_segundo_decendio
-from basedosdados-dev.br_fgv_igp_staging.igp_m_mes as t
+SAFE_CAST(variacao_segundo_decendio AS FLOAT64) variacao_segundo_decendio,
+SAFE_CAST(variacao_acumulada_ano AS FLOAT64) variacao_acumulada_ano,
+SAFE_CAST(indice_fechamento_mensal AS FLOAT64) indice_fechamento_mensal
+FROM basedosdados-dev.br_fgv_igp_staging.igp_m_mes AS t
