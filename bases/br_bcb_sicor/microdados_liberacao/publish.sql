c856ea5a3b216448143efce2b28582cdb69a1afb
Lucas Moreira <lucas.nasc.m@gmail.com>
2022-10-07 09:26:14 -0300

    fix table_config operacao

--- bases/br_bcb_sicor/microdados_liberacao/publish.sql
+++ bases/br_bcb_sicor/microdados_liberacao/publish.sql
@@ -20,9 +20,9 @@ TIPOS:
 CREATE VIEW basedosdados-dev.br_bcb_sicor.microdados_liberacao AS
 SELECT 
 SAFE_CAST(ano AS INT64) ano,
-SAFE_CAST(mes AS STRING) mes,
-SAFE_CAST(data_liberacao AS STRING) data_liberacao,
-SAFE_CAST(valor_liberado AS STRING) valor_liberado,
+SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(data_liberacao AS DATE) data_liberacao,
 SAFE_CAST(id_referencia_bacen AS STRING) id_referencia_bacen,
-SAFE_CAST(numero_ordem AS STRING) numero_ordem
+SAFE_CAST(numero_ordem AS STRING) numero_ordem,
+SAFE_CAST(valor_liberado AS FLOAT64) valor_liberado
 FROM basedosdados-dev.br_bcb_sicor_staging.microdados_liberacao AS t
