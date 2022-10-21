1a5615008e0b0d21f80b6bec60f7da214a6356b6
lucascr91 <lucas.ecomg@gmail.com>
2022-10-21 11:44:53 -0300

    [infra] fix schema website_user

--- bases/br_bd_indicadores/website_user/publish.sql
+++ bases/br_bd_indicadores/website_user/publish.sql
@@ -19,11 +19,12 @@ TIPOS:
 
 CREATE VIEW basedosdados-dev.br_bd_indicadores.website_user AS
 SELECT 
-SAFE_CAST(reference_date AS DATE) reference_date,
-SAFE_CAST(users_1_day AS INT64) users_1_day,
-SAFE_CAST(users_7_days AS INT64) users_7_days,
-SAFE_CAST(users_14_days AS INT64) users_14_days,
-SAFE_CAST(users_28_days AS INT64) users_28_days,
-SAFE_CAST(users_30_days AS INT64) users_30_days,
-SAFE_CAST(new_users AS INT64) new_users
+SAFE_CAST(reference_date AS STRING) reference_date,
+SAFE_CAST(users_1_day AS STRING) users_1_day,
+SAFE_CAST(users_7_days AS STRING) users_7_days,
+SAFE_CAST(users_14_days AS STRING) users_14_days,
+SAFE_CAST(users_28_days AS STRING) users_28_days,
+SAFE_CAST(users_30_days AS STRING) users_30_days,
+SAFE_CAST(new_users AS STRING) new_users,
+SAFE_CAST(active_users AS STRING) active_users
 FROM basedosdados-dev.br_bd_indicadores_staging.website_user AS t
