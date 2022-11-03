77540aba4b31fdaf928e2d300902ce9a27284905
lucascr91 <lucas.ecomg@gmail.com>
2022-11-03 16:57:42 -0300

    [infra] Version 1.7.0 python-package

---
+++ python-package/basedosdados/upload/utils.py
@@ -0,0 +1,160 @@
+''' Utilities functions for Upload sub-module'''
+# pylint: disable=invalid-name,too-many-arguments,too-many-locals,too-many-branches,too-many-statements, protected-access,line-too-long
+import ruamel.yaml as ryaml
+import pandas as pd
+
+import basedosdados as bd
+from basedosdados.exceptions import BaseDosDadosException
+
+def update_columns(table: bd.Table, columns_config_url_or_path: str =None)-> None:
+    """
+    Fills columns in table_config.yaml automatically using a public google sheets URL or a local file. Also regenerate
+    publish.sql and autofill type using bigquery_type.
+
+    The sheet must contain the columns:
+        - name: column name
+        - description: column description
+        - bigquery_type: column bigquery type
+        - measurement_unit: column mesurement unit
+        - covered_by_dictionary: column related dictionary
+        - directory_column: column related directory in the format <dataset_id>.<table_id>:<column_name>
+        - temporal_coverage: column temporal coverage
+        - has_sensitive_data: the column has sensitive data
+        - observations: column observations
+    Args:
+        columns_config_url_or_path (str): Path to the local architeture file or a public google sheets URL.
+            Path only suports csv, xls, xlsx, xlsm, xlsb, odf, ods, odt formats.
+            Google sheets URL must be in the format https://docs.google.com/spreadsheets/d/<table_key>/edit#gid=<table_gid>.
+
+    """
+    ruamel = ryaml.YAML()
+    ruamel.preserve_quotes = True
+    ruamel.indent(mapping=4, sequence=6, offset=4)
+    table_config_yaml = ruamel.load(
+        (table.table_folder / "table_config.yaml").open(encoding="utf-8")
+    )
+
+    if "https://docs.google.com/spreadsheets/d/" in columns_config_url_or_path:
+        if (
+            "edit#gid=" not in columns_config_url_or_path
+            or "https://docs.google.com/spreadsheets/d/"
+            not in columns_config_url_or_path
+            or not columns_config_url_or_path.split("=")[1].isdigit()
+        ):
+            raise BaseDosDadosException(
+                "The Google sheet url not in correct format."
+                "The url must be in the format https://docs.google.com/spreadsheets/d/<table_key>/edit#gid=<table_gid>"
+            )
+        df = table._sheet_to_df(columns_config_url_or_path)
+    else:
+        file_type = columns_config_url_or_path.split(".")[-1]
+        if file_type == "csv":
+            df = pd.read_csv(columns_config_url_or_path, encoding="utf-8")
+        elif file_type in ["xls", "xlsx", "xlsm", "xlsb", "odf", "ods", "odt"]:
+            df = pd.read_excel(columns_config_url_or_path)
+        else:
+            raise BaseDosDadosException(
+                "File not suported. Only csv, xls, xlsx, xlsm, xlsb, odf, ods, odt are supported."
+            )
+
+    df = df.fillna("NULL")
+
+    required_columns = [
+        "name",
+        "bigquery_type",
+        "description",
+        "temporal_coverage",
+        "covered_by_dictionary",
+        "directory_column",
+        "measurement_unit",
+        "has_sensitive_data",
+        "observations",
+    ]
+
+    not_found_columns = required_columns.copy()
+    for sheet_column in df.columns.tolist():
+        for required_column in required_columns:
+            if sheet_column == required_column:
+                not_found_columns.remove(required_column)
+    if not_found_columns:
+        raise BaseDosDadosException(
+            f"The following required columns are not found: {', '.join(not_found_columns)}."
+        )
+
+    columns_parameters = zip(
+        *[df[required_column].tolist() for required_column in required_columns]
+    )
+    for (
+        name,
+        bigquery_type,
+        description,
+        temporal_coverage,
+        covered_by_dictionary,
+        directory_column,
+        measurement_unit,
+        has_sensitive_data,
+        observations,
+    ) in columns_parameters:
+        for col in table_config_yaml["columns"]:
+            if col["name"] == name:
+                col["bigquery_type"] = (
+                    col["bigquery_type"]
+                    if bigquery_type == "NULL"
+                    else bigquery_type.lower()
+                )
+
+                col["description"] = (
+                    col["description"] if description == "NULL" else description
+                )
+
+                col["temporal_coverage"] = (
+                    col["temporal_coverage"]
+                    if temporal_coverage == "NULL"
+                    else [temporal_coverage]
+                )
+
+                col["covered_by_dictionary"] = (
+                    "no"
+                    if covered_by_dictionary == "NULL"
+                    else covered_by_dictionary
+                )
+
+                dataset = directory_column.split(".")[0]
+                col["directory_column"]["dataset_id"] = (
+                    col["directory_column"]["dataset_id"]
+                    if dataset == "NULL"
+                    else dataset
+                )
+
+                table = directory_column.split(".")[-1].split(":")[0]
+                col["directory_column"]["table_id"] = (
+                    col["directory_column"]["table_id"]
+                    if table == "NULL"
+                    else table
+                )
+
+                column = directory_column.split(".")[-1].split(":")[-1]
+                col["directory_column"]["column_name"] = (
+                    col["directory_column"]["column_name"]
+                    if column == "NULL"
+                    else column
+                )
+                col["measurement_unit"] = (
+                    col["measurement_unit"]
+                    if measurement_unit == "NULL"
+                    else measurement_unit
+                )
+
+                col["has_sensitive_data"] = (
+                    "no" if has_sensitive_data == "NULL" else has_sensitive_data
+                )
+
+                col["observations"] = (
+                    col["observations"] if observations == "NULL" else observations
+                )
+
+    with open(table.table_folder / "table_config.yaml", "w", encoding="utf-8") as f:
+        ruamel.dump(table_config_yaml, f)
+
+    # regenerate publish.sql
+    table._make_publish_sql()
---
+++ python-package/tests/test_utils.py
@@ -0,0 +1,40 @@
+"""
+Tests for the upload utilities.
+"""
+# pylint: disable=invalid-name
+from pathlib import Path
+import shutil
+
+import basedosdados as bd
+from basedosdados.upload.utils import update_columns
+
+import pytest
+
+DATASET_ID = "pytest"
+TABLE_ID = "pytest"
+
+TABLE_FILES = ["publish.sql", "table_config.yaml"]
+
+
+def test_update_columns(sample_data, testdir):
+    """
+    Test the update_columns utility.
+    """
+    table = bd.Table(dataset_id=DATASET_ID, table_id=TABLE_ID, metadata_path=testdir)
+    table.create(sample_data, if_table_exists='replace', if_table_config_exists='replace', if_storage_data_exists='replace')
+
+    table_config_path = testdir / DATASET_ID / TABLE_ID / "table_config.yaml"
+    last_modified = table_config_path.stat().st_mtime
+
+    publish_path = testdir / DATASET_ID / TABLE_ID / "publish.sql"
+    last_modified_publish = publish_path.stat().st_mtime
+
+    path_arq = sample_data / "arquitetura_municipio.xlsx"
+    # get path as string
+    path_arq = str(path_arq)
+
+    update_columns(table=table, columns_config_url_or_path=path_arq)
+
+    assert table_config_path.stat().st_mtime > last_modified
+    assert publish_path.stat().st_mtime > last_modified_publish
+    
