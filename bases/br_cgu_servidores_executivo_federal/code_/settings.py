e6947a86a2b928373870ace3178943f7fbd093a9
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-11-08 11:43:39 -0300

    [dados] Feat/br cgu servidores remuneracao (#1369)
    * code to download files
    
    * code to process files
    
    * changing names according to architecture tables
    
    * code to upload tables
    
    * dataset and tables metadata
    
    * corrections metadata
    
    * updating json schema for militares
    
    * adding architecture files for civil
    
    * updating code for servidores civis
    
    * compatibility with architecture file and table create code
    
    * changing temporal coverage in architecture tables
    
    * table of civil employees of the Federal Government
    
    * moving to the old dataset and changing names of tables
    
    * correcting the name of the organization
    
    * changing dataset name to the old name
    
    * renaming achitecture files
    
    * table data after unification with the old dataset (br_cgu_servidores_executivo_federal)
    
    * schemas after publish
    
    * changing architecture - career as partition
    
    * adding column origem in military files to equalize with civil ones
    
    * correcting position of not in negative if clause
    
    * including career partition

---
+++ bases/br_cgu_servidores_executivo_federal/code_/settings.py
@@ -0,0 +1,62 @@
+"""
+Settings for the project.
+"""
+
+# pylint: disable=invalid-name
+
+from pathlib import Path
+
+URL_CGU_DOWNLOADS = "http://www.portaltransparencia.gov.br/download-de-dados/servidores"
+CGU_FILES = {
+    "militar": {"ativo": ["Militares"], "reservista": ["Reserva_Reforma_Militares"]},
+    "civil": {
+        "ativo": ["Servidores_BACEN", "Servidores_SIAPE"],
+        "aposentado": ["Aposentados_BACEN", "Aposentados_SIAPE"],
+    },
+}
+
+TMP_DIR = Path.cwd().resolve().parent / "tmp"
+IN_DIR = Path.cwd().resolve().parent / "input"
+OUT_DIR = Path.cwd().resolve().parent / "output"
+
+REMUNERACAO_COLUMNS = [
+    "ano",
+    "mes",
+    "id_servidor_portal",
+    "cpf",
+    "nome",
+    "remuneracao_bruta_brl",
+    "remuneracao_bruta_usd",
+    "abate_teto_brl",
+    "abate_teto_usd",
+    "gratificao_natalina_brl",
+    "gratificao_natalina_usd",
+    "abate_teto_gratificacao_natalina_brl",
+    "abate_teto_gratificacao_natalina_usd",
+    "ferias_brl",
+    "ferias_usd",
+    "outras_remuneracoes_brl",
+    "outras_remuneracoes_usd",
+    "irrf_brl",
+    "irrf_usd",
+    "pss_rgps_brl",
+    "pss_rgps_usd",
+    "demais_deducoes_brl",
+    "demais_deducoes_usd",
+    "pensao_militar_brl",
+    "pensao_militar_usd",
+    "fundo_saude_brl",
+    "fundo_saude_usd",
+    "taxa_ocupacao_imovel_funcional_brl",
+    "taxa_ocupacao_imovel_funcional_usd",
+    "remuneracao_liquida_militar_brl",
+    "remuneracao_liquida_militar_usd",
+    "verba_indenizatoria_civil_brl",
+    "verba_indenizatoria_civil_usd",
+    "verba_indenizatoria_militar_brl",
+    "verba_indenizatoria_militar_usd",
+    "verba_indenizatoria_deslig_voluntario_brl",
+    "verba_indenizatoria_deslig_voluntario_usd",
+    "total_verba_indenizatoria_brl",
+    "total_verba_indenizatoria_usd",
+]
