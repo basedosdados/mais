b6f9b4b1e3410a1186965c16c71b4c3a9fb6fae6
Arthur Gusmão <arthurgusmao@id.uff.br>
2023-01-03 18:37:59 -0300

    sobe br_me_siconfi_ranking

---
+++ bases/br_me_siconfi/capitais_ranking_qualidade_informacao_fiscal_contabil/publish.sql
@@ -0,0 +1,32 @@
+/*
+Query para publicar a tabela.
+
+Esse é o lugar para:
+    - modificar nomes, ordem e tipos de colunas
+    - dar join com outras tabelas
+    - criar colunas extras (e.g. logs, proporções, etc.)
+
+Qualquer coluna definida aqui deve também existir em `table_config.yaml`.
+
+# Além disso, sinta-se à vontade para alterar alguns nomes obscuros
+# para algo um pouco mais explícito.
+
+TIPOS:
+    - Para modificar tipos de colunas, basta substituir STRING por outro tipo válido.
+    - Exemplo: `SAFE_CAST(column_name AS NUMERIC) column_name`
+    - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
+*/
+
+CREATE VIEW basedosdados-dev.br_me_siconfi.capitais_ranking_qualidade_informacao_fiscal_contabil AS
+SELECT 
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(id_municipio AS STRING) id_municipio,
+SAFE_CAST(indicador_qualidade_informacao_contabil_fiscal AS STRING) indicador_qualidade_informacao_contabil_fiscal,
+SAFE_CAST(posicao_ranking AS INT64) posicao_ranking,
+SAFE_CAST(quantidade_acertos_total AS FLOAT64) quantidade_acertos_total,
+SAFE_CAST(proporcao_acertos AS FLOAT64) proporcao_acertos,
+SAFE_CAST(dimensao_I AS FLOAT64) dimensao_I,
+SAFE_CAST(dimensao_II AS INT64) dimensao_II,
+SAFE_CAST(dimensao_III AS INT64) dimensao_III,
+SAFE_CAST(dimensao_IV AS INT64) dimensao_IV
+FROM basedosdados-dev.br_me_siconfi_staging.capitais_ranking_qualidade_informacao_fiscal_contabil AS t
