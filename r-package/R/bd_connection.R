f9660c591c39028c2aa09f6e04595a27bab22077
Daniel Herszenhut <dhersz@gmail.com>
2023-02-09 13:02:46 -0300

    [r-package] documenta a classe BaseDosDadosConnection e seu metodo

--- r-package/R/bd_connection.R
+++ r-package/R/bd_connection.R
@@ -1,5 +1,15 @@
+#' Base dos dados specific connection to BigQuery
+#'
+#' Implements a connection class to prevent a bug caused when trying to use
+#' BigQueryConnection. The bug is described in Pull Request #1563. Once fixed,
+#' this class and its method should probably be removed.
+#'
 #' @export
 setClass("BaseDosDadosConnection", contains = "BigQueryConnection")
 
+#' @rdname BaseDosDadosConnection-class
+#'
+#' @param con A BaseDosDadosConnection object.
+#'
 #' @export
 dbplyr_edition.BaseDosDadosConnection <- function(con) 2L
