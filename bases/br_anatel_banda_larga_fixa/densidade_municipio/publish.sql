58b0d3895a2d6059e6804b85a74d64250f4b0b0f
tricktx <patrick.tx@hotmail.com.br>
2023-04-13 23:38:03 -0300

    Alterando os schema br_anatel_banda_larga_fixa

--- bases/br_anatel_banda_larga_fixa/densidade_municipio/publish.sql
+++ bases/br_anatel_banda_larga_fixa/densidade_municipio/publish.sql
@@ -23,6 +23,5 @@ SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(id_municipio AS STRING) id_municipio,
-SAFE_CAST(acessos AS INT64) acessos,
 SAFE_CAST(densidade AS FLOAT64) densidade
 FROM basedosdados-dev.br_anatel_banda_larga_fixa_staging.densidade_municipio AS t
--- bases/br_anatel_banda_larga_fixa/densidade_uf/publish.sql
+++ bases/br_anatel_banda_larga_fixa/densidade_uf/publish.sql
@@ -22,6 +22,5 @@ SELECT
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
 SAFE_CAST(sigla_uf AS STRING) sigla_uf,
-SAFE_CAST(acessos AS INT64) acessos,
 SAFE_CAST(densidade AS FLOAT64) densidade
 FROM basedosdados-dev.br_anatel_banda_larga_fixa_staging.densidade_uf AS t
