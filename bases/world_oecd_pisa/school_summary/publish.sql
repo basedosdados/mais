cfe057de7a093f7040a2f75aedd6c92c1f57d8dc
Laura Amaral <laura.amaral@basedosdados.org>
2023-04-14 17:01:19 -0300

    ajust columns

--- bases/world_oecd_pisa/school_summary/publish.sql
+++ bases/world_oecd_pisa/school_summary/publish.sql
@@ -20,16 +20,16 @@ TIPOS:
 CREATE VIEW basedosdados-dev.world_oecd_pisa.school_summary AS
 SELECT 
 SAFE_CAST(year AS INT64) year,
-SAFE_CAST(id_country_iso_3 AS STRING) id_country_iso_3,
+SAFE_CAST(country_id_iso_3 AS STRING) country_id_iso_3,
 SAFE_CAST(school_id AS STRING) school_id,
+SAFE_CAST(public_private AS STRING) public_private,
 SAFE_CAST(funding_government AS FLOAT64) funding_government,
 SAFE_CAST(funding_fees AS FLOAT64) funding_fees,
 SAFE_CAST(funding_donation AS FLOAT64) funding_donation,
-SAFE_CAST(enrol_boys AS INT64) enrol_boys,
-SAFE_CAST(enrol_girls AS INT64) enrol_girls,
+SAFE_CAST(number_boys AS INT64) number_boys,
+SAFE_CAST(number_girls AS INT64) number_girls,
+SAFE_CAST(number_students AS INT64) number_students,
 SAFE_CAST(student_teacher_ratio AS FLOAT64) student_teacher_ratio,
-SAFE_CAST(public_private AS STRING) public_private,
 SAFE_CAST(staff_shortage AS FLOAT64) staff_shortage,
-SAFE_CAST(school_weight AS FLOAT64) school_weight,
-SAFE_CAST(school_size AS INT64) school_size
+SAFE_CAST(school_weight AS FLOAT64) school_weight
 FROM basedosdados-dev.world_oecd_pisa_staging.school_summary AS t
