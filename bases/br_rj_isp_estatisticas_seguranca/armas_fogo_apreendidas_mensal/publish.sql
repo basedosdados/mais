46e8f9ce2c9621fae5640c1dcbffbfb3a676acb3
folhesgabriel <pisagabriel09@gmail.com>
2023-04-20 16:30:33 -0300

    fix: corrige publish

--- bases/br_rj_isp_estatisticas_seguranca/armas_fogo_apreendidas_mensal/publish.sql
+++ bases/br_rj_isp_estatisticas_seguranca/armas_fogo_apreendidas_mensal/publish.sql
@@ -25,5 +25,5 @@ SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(id_cisp AS STRING) id_cisp,
 SAFE_CAST(id_aisp AS STRING) id_aisp,
 SAFE_CAST(id_risp AS STRING) id_risp,
-SAFE_CAST(quantidade_arma_fogo_apreendida AS STRING) quantidade_arma_fogo_apreendida
+SAFE_CAST(quantidade_arma_fogo_apreendida AS INT64) quantidade_arma_fogo_apreendida
 FROM basedosdados-dev.br_rj_isp_estatisticas_seguranca_staging.armas_fogo_apreendidas_mensal AS t
