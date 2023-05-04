46617e57734bd744a1c540e5e1c824b3749cf9fb
folhesgabriel <pisagabriel09@gmail.com>
2023-05-04 16:44:56 -0300

    fix: make requested changes

--- bases/br_bcb_sicor/recurso_publico_cooperado/publish.sql
+++ bases/br_bcb_sicor/recurso_publico_cooperado/publish.sql
@@ -23,5 +23,5 @@ SAFE_CAST(id_referencia_bacen AS STRING) id_referencia_bacen,
 SAFE_CAST(numero_ordem AS STRING) numero_ordem,
 SAFE_CAST(tipo_cpf_cnpj AS STRING) tipo_cpf_cnpj,
 SAFE_CAST(tipo_pessoa AS STRING) tipo_pessoa,
-SAFE_CAST(valor_parcela AS STRING) valor_parcela
+SAFE_CAST(valor_parcela AS FLOAT64) valor_parcela
 FROM basedosdados-dev.br_bcb_sicor_staging.recurso_publico_cooperado AS t
