2163160e861f7c99edb58c8211926c7807d07528
Pedro Castro <aspeddro@gmail.com>
2024-07-08 10:01:11 -0300

    Close #1718

--- python-package/basedosdados/upload/table.py
+++ python-package/basedosdados/upload/table.py
@@ -561,7 +561,13 @@ class Table(Base):
                 Path,
             ),
         ):
-            Storage(self.dataset_id, self.table_id).upload(
+            Storage(
+                dataset_id=self.dataset_id,
+                table_id=self.table_id,
+                config_path=self.config_path,
+                bucket_name=self.bucket_name,
+                overwrite_cli_config=self.overwrite_cli_config,
+            ).upload(
                 path=path,
                 mode="staging",
                 if_exists=if_storage_data_exists,
