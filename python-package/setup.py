1918719bcbacf4bf0a308e77146915f1dea4f660
d116626 <d116626@gmail.com>
2024-07-24 12:39:32 -0300

    fix: log in wrong position

--- python-package/setup.py
+++ python-package/setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="basedosdados",
-    version="2.0.0-beta.15",
+    version="2.0.0-beta.19",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
